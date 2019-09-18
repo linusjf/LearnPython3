ba73cbcd7aa9d06e68b8248d2e1826357801438e
fernal73 <linus.fernandes@gmail.com>
2019-09-18 18:37:08 +0530

    Added more scripts for best practices

---
+++ execution_methods.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+print("This is my file to test Python's execution methods.")
+print("The variable __name__ tells me which context this file is running in.")
+print("The value of __name__ is:", repr(__name__))
