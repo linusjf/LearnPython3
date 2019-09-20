2114226de3f5f8e7ad1a97524b54e93682ae576c
fernal73 <linus.fernandes@gmail.com>
2019-09-20 16:24:39 +0530

    Linted

--- best_practices.py
+++ best_practices.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Best practices"""
 from time import sleep
 
 print("This is my file to demonstrate best practices.")
 
 def process_data(data):
+    """Process data function"""
     print("Beginning data processing...")
     modified_data = data + " that has been modified"
     sleep(3)
@@ -11,15 +13,18 @@ def process_data(data):
     return modified_data
 
 def read_data_from_web():
+    """Read data from web function"""
     print("Reading data from the Web")
     data = "Data from the web"
     return data
 
 def write_data_to_database(data):
+    """write data to database function"""
     print("Writing data to a database")
     print(data)
 
 def main():
+    """main function"""
     data = read_data_from_web()
     modified_data = process_data(data)
     write_data_to_database(modified_data)
--- execution_methods.py
+++ execution_methods.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Execution methods"""
 print("This is my file to test Python's execution methods.")
 print("The variable __name__ tells me which context this file is running in.")
 print("The value of __name__ is:", repr(__name__))
