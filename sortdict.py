6e4b758aaabbdc9d8ad63374a95241dae1a85987
fernal73 <linus.fernandes@gmail.com>
2019-09-20 18:54:04 +0530

    linted

--- sortdict.py
+++ sortdict.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
+"""Sort dictionary"""
+import operator
+XS = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
 
-xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
-
-print(sorted(xs.items(), key=lambda x: x[1]))
+print(sorted(XS.items(), key=lambda x: x[1]))
 
 # Or:
 
-import operator
-print(sorted(xs.items(), key=operator.itemgetter(1)))
+print(sorted(XS.items(), key=operator.itemgetter(1)))
 
-xs = {"bash":"unix", "command":"windows","powershell":"windows","korn":"unix"}
-print(sorted(xs.items(), key=lambda x: x[0]))
-print(sorted(xs.items(), key=lambda x: x[1]))
+XS = {"bash":"unix", "command":"windows", "powershell":"windows", "korn":"unix"}
+print(sorted(XS.items(), key=lambda x: x[0]))
+print(sorted(XS.items(), key=lambda x: x[1]))
--- tentiptricks.py
+++ tentiptricks.py
@@ -1,103 +1,95 @@
 #!/usr/bin/env python3
+"""Ten Tip Tricks"""
+import os
+import socket
+import sys
+from collections import Counter
+
 # 1. In-Place Swapping Of Two Numbers.
-x, y = 10, 20
+X, y = 10, 20
 
-print(x, y) 
+print(X, y)
 
-x, y = y, x 
+X, y = y, X
 
-print(x, y) 
+print(X, y)
 
 # 2. Reversing a string in Python
 
+_a = "GeeksForGeeks"
 
-a ="GeeksForGeeks"
-
-print("Reverse is", a[::-1]) 
+print("Reverse is", _a[::-1])
 
 # 3. Create a single string from all the elements in list
 
-a = ["Geeks", "For", "Geeks"] 
+_a = ["Geeks", "For", "Geeks"]
 
-print(" ".join(a)) 
+print(" ".join(_a))
 
 # 4. Chaining Of Comparison Operators.
 
+_n = 10
 
-n = 10
+result = 1 < _n < 20
 
-result = 1 < n < 20
+print(result)
 
-print(result) 
+result = 1 > _n <= 9
 
-result = 1 > n <= 9
-
-print(result) 
+print(result)
 
 # 4. Print The File Path Of Imported Modules.
 
-import os; 
+print(os)
 
-import socket; 
-
-print(os) 
-
-print(socket) 
+print(socket)
 
 # 5. Use Of Enums In Python.
 
+class MyName:
+    """Enum definition"""
+    Geeks, For, Geeks = range(3)
 
-class MyName: 
-
-    Geeks, For, Geeks = range(3) 
-
-print(MyName.Geeks) 
-
-print(MyName.For) 
-
-print(MyName.Geeks) 
+print(MyName.Geeks)
+print(MyName.For)
+print(MyName.Geeks)
 
 # 6. Return Multiple Values From Functions.
 
-def x(): 
-
+def x():
+    """return multiple values"""
     return 1, 2, 3, 4
 
-a, b, c, d = x() 
+a, b, c, d = x()
 
-print(a, b, c, d) 
+print(a, b, c, d)
 
 # 7. Find The Most Frequent Value In A List.
 
-test = [1, 2, 3, 4, 2, 2, 3, 1, 4, 4, 4] 
+test = [1, 2, 3, 4, 2, 2, 3, 1, 4, 4, 4]
 
-print(max(set(test), key = test.count)) 
+print(max(set(test), key=test.count))
 
 # 8. Check The Memory Usage Of An Object.
 
-import sys 
+X = 1
 
-x = 1
-
-print(sys.getsizeof(x)) 
+print(sys.getsizeof(X))
 
 # 9. Print string N times.
 
-n = 2; 
+n = 2
 
-a ="GeeksforGeeks"; 
+a = "GeeksforGeeks"
 
-print(a * n); 
+print(a * n) 
 
 # 10. Checking if two words are anagrams
 
-from collections import Counter 
-
 def is_anagram(str1, str2): 
+    """check if anagram"""
+    return Counter(str1) == Counter(str2) 
 
-     return Counter(str1) == Counter(str2) 
-
-print(is_anagram('geek', 'eegk')) 
+print(is_anagram('geek', 'eegk'))
 
 print(is_anagram('geek', 'peek'))     
-
