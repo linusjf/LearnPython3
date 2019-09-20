67ff05dc37a7375e4055c395d2336dcbab37a335
fernal73 <linus.fernandes@gmail.com>
2019-09-20 15:35:16 +0530

    Linted

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""First program in Python3"""
+
+def main():
+    """Main function"""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
+
+print("Guru99")
+
+print("Hello, World! Again!")
