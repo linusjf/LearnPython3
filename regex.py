ce6b020ac4d59c82f40d362fdb0de857bfd1cf61
fernal73 <linus.fernandes@gmail.com>
2019-09-24 08:30:53 +0530

    Added regex

--- regex.py
+++ regex.py
@@ -6,14 +6,30 @@ R1 = re.findall(r"^\w+", XX)
 print(R1)
 print((re.split(r'\s', 'we are splitting the words')))
 print((re.split(r's', 'split the words')))
-words = ["guru99 get", "guru99 give", "guru Selenium"]
+WORDS = ["guru99 get", "guru99 give", "guru Selenium"]
 
-for element in words:
-    z = re.match("(g\w+)\W(g\w+)", element)
+for element in WORDS:
+    z = re.match(r"(g\w+)\W(g\w+)", element)
     if z:
         print((z.groups()))
 
-for element in words:
-    z = re.match("(g\w+)\W(S\w+)", element)
+for element in WORDS:
+    z = re.match(r"(g\w+)\W(S\w+)", element)
     if z:
         print((z.groups()))
+
+PATTERNS = ['software testing', 'guru99']
+TEXT = 'software testing is fun?'
+for pattern in PATTERNS:
+    print('Looking for "%s" in "%s" ->' % (pattern, TEXT), end=' ')
+    if re.search(pattern, TEXT):
+        print('found a match!')
+    else:
+        print('no match')
+
+ABC = """guru99@google.com,
+careerguru99@hotmail.com,
+users@yahoomail.com"""
+EMAILS = re.findall(r'[\w\.-]+@[\w\.-]+', ABC)
+for email in EMAILS:
+    print(email)
