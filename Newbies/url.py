0ca7b80842ede65e7770d5fa780e0c5c645c78be
fernal73 <linus.fernandes@gmail.com>
2019-09-26 04:11:46 +0530

    Url

---
+++ Newbies/url.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""URL Loading"""
+#
+# read the data from the URL and print it
+#
+import urllib.request
+# open a connection to a URL using urllib
+WEBURL = urllib.request.urlopen('https://www.youtube.com/user/guru99com')
+
+# get the result code and print it
+print("result code: " + str(WEBURL.getcode()))
+
+# read the data from the URL and print it
+DATA = WEBURL.read()
+print(DATA)
