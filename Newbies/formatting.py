6f7b40f4f910771c2b72383a6e7c2511e1752ea3
fernal73 <linus.fernandes@gmail.com>
2019-09-28 15:40:34 +0530

    Formated and fixed

--- Newbies/formatting.py
+++ Newbies/formatting.py
@@ -21,6 +21,6 @@ print('{0:>8} | {1:>8}'.format('Vegetable', 'Quantity'))
 print('{0:>9} | {1:>8.2f}'.format('Asparagus', 2.33333))
 print('{0:>9} | {1:>8.2f}'.format('Onions', 10))
 
-vegetable = input('Enter a name of a vegetable: ')
+VEGETABLE = input('Enter a name of a vegetable: ')
 print()
-print('{} is a lovely vegetable.'.format(vegetable))
+print('{} is a lovely vegetable.'.format(VEGETABLE))
