466ba0a962e75460384b7a1adf812bd4bd9fd9a8
fernal73 <linus.fernandes@gmail.com>
2019-09-28 10:21:27 +0530

    Added pigdqueal

---
+++ Newbies/pigsqueal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""The pig speaks."""
+TEXT = input('What would you like the pig to say? ')
+TEXT_LENGTH = len(TEXT)
+print()
+print('             {}'.format('_' * TEXT_LENGTH))
+print('           < {} >'.format(TEXT))
+print('             {}'.format('-' * TEXT_LENGTH))
+print('           /')
+print('     ^..^ /')
+print('~( ( oo )')
+print(' ,, ,,')
