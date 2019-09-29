e6a499c7394c88cff47ded86ece8206a3d785b9c
fernal73 <linus.fernandes@gmail.com>
2019-09-29 11:22:54 +0530

    Modified function so that it doesn't run indefinitely

--- Intermediate/generator.py
+++ Intermediate/generator.py
@@ -18,10 +18,37 @@ def finite_sequence(limit):
         num += 1
 
 
+def is_palindrome(num):
+    """Check if palindrome."""
+    # Skip single-digit inputs
+    if num // 10 == 0:
+        return False
+    temp = num
+    reversed_num = 0
+
+    while temp != 0:
+        reversed_num = (reversed_num * 10) + (temp % 10)
+        temp = temp // 10
+
+    if num == reversed_num:
+        return num
+
+
 def run_finite_seq(rand):
     """Run finite sequence."""
     for i in finite_sequence(rand):
         print(i, end=" ")
+
+
+def run_finite_palindromes_sixdigits(rand):
+    """Run finite sequence of palindromes."""
+    for i in finite_sequence(rand):
+        pal = is_palindrome(i)
+        if pal:
+            if len(str(pal)) <= 6:
+                print(pal)
+            else:
+                return
 
 
 CSV_GEN = csv_reader("techcrunch.csv")
@@ -42,4 +69,4 @@ print(f"Row count is {ROW_COUNT}")
 
 RAND = random.randint(0, sys.maxsize)
 
-run_finite_seq(RAND)
+run_finite_palindromes_sixdigits(RAND)
