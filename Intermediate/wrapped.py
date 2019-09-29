8c877f5cd92ce31f6ce34e98a3e0cfe905d131a4
fernal73 <linus.fernandes@gmail.com>
2019-09-29 19:01:01 +0530

    Added LockedIterator workable

--- Intermediate/wrapped.py
+++ Intermediate/wrapped.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 """Locked Iterator."""
-import threading
+from threading import Lock
+from collections.abc import Iterator
 
-class LockedIterator():
+# pylint: disable=too-few-public-methods
+class LockedIterator(Iterator):
     """Locked Iterator."""
     def __init__(self, _it):
-        self.lock = threading.Lock()
+        self.lock = Lock()
         self._it = _it.__iter__()
 
-    def __iter__(self):
-        yield self
-
-    def next(self):
+    def __next__(self):
         """Return next."""
         self.lock.acquire()
         try:
-            return self._it.next()
+            return self._it.__next__()
         finally:
             self.lock.release()
 
+# pylint: enable=too-few-public-methods
+
 GEN = [x*2 for x in [1, 2, 3, 4]]
+
+for _ in GEN:
+    print(_)
 
 G2 = LockedIterator(GEN)
 for _ in G2:
