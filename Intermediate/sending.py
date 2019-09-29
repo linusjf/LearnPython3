2ef206002297764ff63aa50c1a3c804d3aac4868
fernal73 <linus.fernandes@gmail.com>
2019-09-30 04:56:27 +0530

    Added sending negative

--- Intermediate/sending.py
+++ Intermediate/sending.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Infinite looper with send."""
 
+
 def infinite_looper(objects):
     """Loop indefinitely."""
     count = 0
@@ -13,7 +14,9 @@ def infinite_looper(objects):
         else:
             count += 1
 
+
 X = infinite_looper("A string with some words")
 print(next(X))
 print(X.send(9))
 print(X.send(12))
+print(X.send(-10))
