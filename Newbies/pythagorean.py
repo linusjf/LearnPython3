afc1eaef5f2f3ef9846dbdccb06e112eca9b83d7
fernal73 <linus.fernandes@gmail.com>
2019-09-30 05:30:46 +0530

    Added new file

---
+++ Newbies/pythagorean.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Pythagorean triplets."""
+from math import sqrt
+N = input("Maximum Number? ")
+print()
+print("Number input: " + N)
+N = int(N)+1
+for a in range(1, N):
+    for b in range(a, N):
+        c_square = a**2 + b**2
+        c = int(sqrt(c_square))
+        if c_square - c**2 == 0:
+            print(a, b, c)
