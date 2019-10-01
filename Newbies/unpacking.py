ccdb9ea50c89a51e7d518d70e0ad2ec9a1deba6b
fernal73 <linus.fernandes@gmail.com>
2019-10-01 09:55:19 +0530

    Added python trick function unpacking

---
+++ Newbies/unpacking.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Unpacking function parameters."""
+
+
+def myfunc(_x, _y, _z):
+    """Print function parameters."""
+    print(_x, _y, _z)
+
+
+TUPLE_VEC = (1, 0, 1)
+DICT_VEC = {'_x': 1, '_y': 0, '_z': 1}
+
+myfunc(*TUPLE_VEC)
+myfunc(**DICT_VEC)
