26f1afe9eb760e49b079caf90c373225aaddd5ca
fernal73 <linus.fernandes@gmail.com>
2019-10-04 11:13:15 +0530

    Timing

---
+++ Newbies/timing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Timing little Python code."""
+
+import timeit
+
+print(timeit.timeit('"-".join(str(n) for n in range(100))', number=10000))
+print(timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000))
+print(timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000))
+print(timeit.timeit('"-".join(map(str, range(100)))', number=10000))
+print(timeit.timeit('"-".join(map(str, range(100)))', number=10000))
