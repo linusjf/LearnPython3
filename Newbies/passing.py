a23c2a30136eefd7f50bb42fde80a84eedfb3991
fernal73 <linus.fernandes@gmail.com>
2019-10-15 14:22:12 +0530

    Added sentdict

--- Newbies/passing.py
+++ Newbies/passing.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Passing functions as objects."""
+
+
 def myfunc(_a, _b):
     """Add two numbers."""
     return _a + _b
+
 
 FUNCS = [myfunc]
 print(FUNCS[0])
---
+++ Newbies/sentdict.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Creating a dictionary from sentence."""
+import re
+STRING = """Creating a dictionary and updating it with word count.
+We will discuss other methods of updating a dictionary later."""
+
+TOKENS = re.split(r"[\s\.\n]\s*", STRING)
+
+COUNT = {}
+
+for WORD in TOKENS:
+    if WORD:
+        COUNT[WORD] = COUNT.get(WORD, 0) + 1
+
+print(COUNT)
