d4f15047dcd85b9e3a4e600b049cc7a2475cdc32
fernal73 <linus.fernandes@gmail.com>
2021-08-10 03:06:07 +0530

    Add stringdict

---
+++ Kaggle/stringdict.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+x = 'Pluto is a planet'
+y = "Pluto is a planet"
+print(x == y)
+
+print("Pluto's a planet!")
+print('My dog is named "Pluto"')
+
+hello = "hello\nworld"
+print(hello)
+
+triplequoted_hello = """hello
+world"""
+print(triplequoted_hello)
+print(triplequoted_hello == hello)
+
+print("hello")
+print("world")
+print("hello", end = '')
+print("pluto", end = '')
+print("")
