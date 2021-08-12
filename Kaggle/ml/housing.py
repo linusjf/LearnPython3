750289ea77f4e36c1b2c63e56a5720c274960009
fernal73 <linus.fernandes@gmail.com>
2021-08-12 10:55:26 +0530

    Add for mae for different leaf nodes

--- Kaggle/ml/housing.py
+++ Kaggle/ml/housing.py
@@ -70,3 +70,15 @@ melbourne_model.fit(train_X, train_y)
 val_predictions = melbourne_model.predict(val_X)
 print("Mean Absolute Error (Split):")
 print(mean_absolute_error(val_y, val_predictions))
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
