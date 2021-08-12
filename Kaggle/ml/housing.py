19783b8ae5d8501282e6a422573af6ae170c1340
fernal73 <linus.fernandes@gmail.com>
2021-08-12 10:37:56 +0530

    Add mae for data split

--- Kaggle/ml/housing.py
+++ Kaggle/ml/housing.py
@@ -52,3 +52,21 @@ from sklearn.metrics import mean_absolute_error
 predicted_home_prices = melbourne_model.predict(X)
 print("Mean Absolute Error:")
 print(mean_absolute_error(y, predicted_home_prices))
+
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(train_X, train_y)
+
+# get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_X)
+print("Mean Absolute Error (Split):")
+print(mean_absolute_error(val_y, val_predictions))
