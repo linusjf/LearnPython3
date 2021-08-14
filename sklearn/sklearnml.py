4e714de01bb463d2e1e4041737d52cfae4bb4468
fernal73 <linus.fernandes@gmail.com>
2021-08-14 11:55:18 +0530

    refactor to use with statement

--- sklearn/sklearnml.py
+++ sklearn/sklearnml.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from matplotlib.backends.backend_pdf import PdfPages
-pp = PdfPages('sklearnml.pdf')
 
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
@@ -17,11 +16,47 @@ print(digits.data)
 print(digits.target)
 print(digits.images[0])
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    ax.set_title('Training: %i' % label)
+with PdfPages('sklearnml.pdf') as pp:
+    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    for ax, image, label in zip(axes, digits.images, digits.target):
+        ax.set_axis_off()
+        ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+        ax.set_title('Training: %i' % label)
 
-pp.savefig()
-pp.close()
+    pp.savefig()
+    plt.close()
+    # flatten the images
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+
+    # Create a classifier: a support vector classifier
+    clf = svm.SVC(gamma=0.001)
+
+    # Split data into 50% train and 50% test subsets
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, digits.target, test_size=0.5, shuffle=False)
+
+    # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
+
+    # Predict the value of the digit on the test subset
+    predicted = clf.predict(X_test)
+
+    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    for ax, image, prediction in zip(axes, X_test, predicted):
+        ax.set_axis_off()
+        image = image.reshape(8, 8)
+        ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+        ax.set_title(f'Prediction: {prediction}')
+
+    pp.savefig()
+    plt.close()
+    print(f"Classification report for classifier {clf}:\n"
+          f"{metrics.classification_report(y_test, predicted)}\n")
+
+    disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
+    disp.figure_.suptitle("Confusion Matrix")
+    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+
+    pp.savefig()
+    plt.close()
