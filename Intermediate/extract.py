37eb0657e84c4ac81a364f1906493a6148877c16
fernal73 <linus.fernandes@gmail.com>
2021-08-16 21:58:14 +0530

    handle case of beginning with currency other than $

--- Intermediate/extract.py
+++ Intermediate/extract.py
@@ -4,15 +4,17 @@
 from price_parser import parse_price
 
 def extract_price(description):
-    if description.find(",") == -1:
-        return parse_price(description).amount
-    else:
+    if description.startswith("€") == -1:
         return parse_price(description,decimal_separator=",").amount
+    else:
+        return parse_price(description).amount
 
 usdprice = "$24.99"
 itprice = "€24,99"
+usprice2 = "$24,990"
 frprice = "24€99"
 
 print(extract_price(usdprice))
 print(extract_price(itprice))
 print(extract_price(frprice))
+print(extract_price(usprice2))
