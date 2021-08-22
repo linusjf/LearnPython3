da79de66fed457f25f23d8a6623850a0c98c149e
fernal73 <linus.fernandes@gmail.com>
2021-08-22 12:54:08 +0530

    Add more group sort methods

--- Kaggle/pandas/groupsort.py
+++ Kaggle/pandas/groupsort.py
@@ -14,3 +14,19 @@ print(reviews.groupby('winery').apply(lambda df: df.title.iloc[0]))
 print(reviews.groupby(['country', 'province']).apply(lambda df: df.loc[df.points.idxmax()]))
 
 print(reviews.groupby(['country']).price.agg([len, min, max]))
+
+countries_reviewed = reviews.groupby(['country', 'province']).description.agg([len])
+print(countries_reviewed)
+
+mi = countries_reviewed.index
+print(type(mi))
+print(countries_reviewed.reset_index())
+
+countries_reviewed = countries_reviewed.reset_index()
+print(countries_reviewed.sort_values(by='len'))
+
+print(countries_reviewed.sort_values(by='len', ascending=False))
+
+print(countries_reviewed.sort_index())
+
+print(countries_reviewed.sort_values(by=['country', 'len']))
