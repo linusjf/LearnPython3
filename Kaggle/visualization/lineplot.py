1ff49c7375b60f98271c4b770ce9400919ecb3f0
fernal73 <linus.fernandes@gmail.com>
2021-08-22 17:49:20 +0530

    Add hello sb and line plot

---
+++ Kaggle/visualization/lineplot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+pd.set_option("display.max_rows", 5)
+pd.plotting.register_matplotlib_converters()
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages('lineplot.pdf')
+print("Setup Complete")
+
+flights = sns.load_dataset("flights")
+print(flights.head())
+
+may_flights = flights.query("month == 'May'")
+sns.lineplot(data=may_flights, x="year", y="passengers")
+
+pp.savefig()
+
+flights_wide = flights.pivot("year", "month", "passengers")
+print(flights_wide.head())
+
+sns.lineplot(data=flights_wide["May"])
+
+pp.savefig()
+
+sns.lineplot(data=flights_wide)
+
+pp.savefig()
+pp.close()
