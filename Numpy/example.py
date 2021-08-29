3b7f534dfb43ce62f551380915da6c6a9fd8f8b4
fernal73 <linus.fernandes@gmail.com>
2021-08-29 18:35:36 +0530

    Add example.py

---
+++ Numpy/example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+a = np.arange(15).reshape(3, 5)
+print(a)
+print(a.shape)
+print(a.ndim)
+print(a.dtype.name)
+print(a.itemsize)
+print(a.size)
+print(type(a))
+b = np.array([6, 7, 8])
+print(b)
+print(type(b))
