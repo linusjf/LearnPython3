dc6a5815d2a36d67d6bca5e76602fe38c62327bb
fernal73 <linus.fernandes@gmail.com>
2021-08-30 09:43:31 +0530

    Add titanic category plots

--- Kaggle/visualization/catplots.py
+++ Kaggle/visualization/catplots.py
@@ -120,6 +120,31 @@ plot = sns.catplot(x="class", y="survived", hue="sex",
 pp.savefig()
 plot.figure.clear()
 
+g = sns.catplot(x="alive", col="deck", col_wrap=4,
+                data=titanic[titanic.deck.notnull()],
+                kind="count", height=2.5, aspect=.8)
+pp.savefig()
+plot.figure.clear()
+
+g = sns.catplot(x="age", y="embark_town",
+                hue="sex", row="class",
+                data=titanic[titanic.embark_town.notnull()],
+                orient="h", height=2, aspect=3, palette="Set3",
+                kind="violin", dodge=True, cut=0, bw=.2)
+pp.savefig()
+plot.figure.clear()
+
+g = sns.catplot(x="who", y="survived", col="class",
+                data=titanic, saturation=.5,
+                kind="bar", ci=None, aspect=.6)
+(g.set_axis_labels("", "Survival Rate")
+  .set_xticklabels(["Men", "Women", "Children"])
+  .set_titles("{col_name} {col_var}")
+  .set(ylim=(0, 1))
+  .despine(left=True))
+pp.savefig()
+plot.figure.clear()
+
 iris = sns.load_dataset("iris")
 print(iris.head())
 plot = sns.catplot(data=iris, orient="h", kind="box")
@@ -151,6 +176,22 @@ g.figure.clear()
 exercise = sns.load_dataset("exercise")
 print(exercise.head())
 g = sns.catplot(x="time", y="pulse", hue="kind", data=exercise)
+pp.savefig()
+g.figure.clear()
+
+g = sns.catplot(x="time", y="pulse", hue="kind",
+               data=exercise, kind="violin")
+pp.savefig()
+g.figure.clear()
+
+g = sns.catplot(x="time", y="pulse", hue="kind",
+                col="diet", data=exercise)
+pp.savefig()
+g.figure.clear()
+
+g = sns.catplot(x="time", y="pulse", hue="kind",
+                col="diet", data=exercise,
+                height=5, aspect=.8)
 pp.savefig()
 g.figure.clear()
 
