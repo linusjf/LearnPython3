dca1d6777b1911a4c60f9b75330cf273a3557645
fernal73 <linus.fernandes@gmail.com>
2021-09-01 20:54:42 +0530

    Add numerical operations example

--- Numpy/idxsli.py
+++ Numpy/idxsli.py
@@ -69,3 +69,33 @@ c = a[::2].copy()
 c[0] = 12
 print(a)
 print(np.may_share_memory(a, c))
+
+np.random.seed(3)
+a = np.random.randint(0, 21, 15)
+print(a)
+print((a % 3 == 0))
+mask = (a % 3 == 0)
+# or, a[a%3==0]
+extract_from_a = a[mask] 
+# extract a sub-array with the mask
+print(extract_from_a)
+a[a % 3 == 0] = -1
+print(a)
+a = np.arange(0, 100, 10)
+print(a)
+# note: [2, 3, 2, 4, 2] is a Python list
+a[[2, 3, 2, 4, 2]] 
+a[[9, 7]] = -100
+print(a)
+
+a = np.arange(10)
+idx = np.array([[3, 4], [9, 7]])
+print(idx.shape)
+print(a[idx])
+
+a = np.arange(6) + np.arange(0, 51, 10)[:, np.newaxis]
+print(a)
+print(a[(0,1,2,3,4), (1,2,3,4,5)])
+print(a[3:, [0,2,5]])
+mask = np.array([1,0,1,0,0,1], dtype=bool)
+print(a[mask, 2])
