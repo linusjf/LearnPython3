52fef041ba47080c45ef8c79d4feabc87b0bc486
fernal73 <linus.fernandes@gmail.com>
2021-09-01 08:33:06 +0530

    add more plots

--- Numpy/matplots.py
+++ Numpy/matplots.py
@@ -3,6 +3,7 @@
 
 # the tidy way
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages('matplots.pdf')
@@ -20,6 +21,19 @@ pp.savefig()
 plt.clf()
 
 image = np.random.rand(30, 30)
+plt.imshow(image, cmap=plt.cm.hot)
+plt.colorbar()
+pp.savefig()
+plt.clf()
+
+x = np.arange(0,10*np.pi,0.1)   # start,stop,step
+y = np.cos(x)
+plt.plot(x, y)
+pp.savefig()
+plt.clf()
+image = np.linspace(-1.0, 1.0,num = 900, endpoint = True)
+random.shuffle(image)
+image = image.reshape(30,30)
 plt.imshow(image, cmap=plt.cm.hot)
 plt.colorbar()
 pp.savefig()
