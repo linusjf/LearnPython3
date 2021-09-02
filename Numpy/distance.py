95f6aa67098b0b133340866fd76f53404879ae59
fernal73 <linus.fernandes@gmail.com>
2021-09-02 22:47:14 +0530

    Add distance

---
+++ Numpy/distance.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages('distance.pdf')
+print("Setup Complete")
+
+mileposts = np.array([0, 198, 303, 736, 871, 1175, 1475, 1544,
+                      1913, 2448])
+distance_array = np.abs(mileposts - mileposts[:, np.newaxis])
+print(distance_array)
+
+x, y = np.arange(5), np.arange(5)[:, np.newaxis]
+distance = np.sqrt(x ** 2 + y ** 2)
+print(distance)
+plt.pcolor(distance)
+plt.colorbar()
+
+pp.savefig()
+plt.clf()
+
+pp.close()
