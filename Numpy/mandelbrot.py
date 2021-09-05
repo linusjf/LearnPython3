eb765c3a382ba5d5cd5f5d49f5a44ded251b7ebc
fernal73 <linus.fernandes@gmail.com>
2021-09-05 14:51:57 +0530

    add mandelbrot. start matplotlib samples

---
+++ Numpy/mandelbrot.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import newaxis
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages('mandelbrot.pdf')
+print("Setup Complete")
+
+def compute_mandelbrot(N_max, some_threshold, nx, ny):
+    # A grid of c-values
+    x = np.linspace(-2, 1, nx)
+    y = np.linspace(-1.5, 1.5, ny)
+
+    c = x[:,newaxis] + 1j*y[newaxis,:]
+
+    # Mandelbrot iteration
+    z = c
+
+    # The code below overflows in many regions of the x-y grid, suppress
+    # warnings temporarily
+    with np.warnings.catch_warnings():
+        np.warnings.simplefilter("ignore")
+        for j in range(N_max):
+            z = z**2 + c
+        mandelbrot_set = (abs(z) < some_threshold)
+
+    return mandelbrot_set
+
+mandelbrot_set = compute_mandelbrot(50, 50., 601, 401)
+
+plt.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5])
+plt.gray()
+plt.show()
+pp.savefig()
+pp.close()
