d388dd301afe31fc68a227bebebfeb9b4af09103
fernal73 <linus.fernandes@gmail.com>
2021-09-05 16:40:55 +0530

    Add annotations

--- Matplotlib/custom.py
+++ Matplotlib/custom.py
@@ -35,6 +35,21 @@ ax.xaxis.set_ticks_position('bottom')
 ax.spines['bottom'].set_position(('data',0))
 ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data',0))
+
+#Annotate
+t = 2 * np.pi / 3
+plt.plot([t, t], [0, np.cos(t)], color='blue', linewidth=2.5, linestyle="--")
+plt.scatter([t, ], [np.cos(t), ], 50, color='blue')
+plt.annotate(r'$cos(\frac{2\pi}{3} )=-\frac{1} {2} $',
+xy=(t, np.cos(t)), xycoords='data',
+xytext=(-90, -50), textcoords='offset points', fontsize=16,
+arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+plt.plot([t, t],[0, np.sin(t)], color='red', linewidth=2.5, linestyle="--")
+plt.scatter([t, ],[np.sin(t), ], 50, color='red')
+plt.annotate(r'$sin(\frac{2\pi}{3} )=\frac{\sqrt{3} }{2} $',
+xy=(t, np.sin(t)), xycoords='data',
+xytext=(+10, +30), textcoords='offset points', fontsize=16,
+arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
 # Save figure using 72 dots per inch
 plt.savefig("custom.png", dpi=72)
 # Show result on screen
