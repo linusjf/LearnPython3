0028e9e278ab1624839a8717d357ba1f63e330ca
fernal73 <linus.fernandes@gmail.com>
2021-09-06 09:10:06 +0530

    Add ball_angle

---
+++ Computations/ball_angle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from math import atan,pi
+
+# Horizontal position
+x = 10             
+# Vertical position
+y = 10             
+
+angle = atan(y/x)
+
+print((angle/pi)*180)
