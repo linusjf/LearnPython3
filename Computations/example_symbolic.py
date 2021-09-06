de72b2d41f05ff57fb3602ab4a8c0c16cefdef72
fernal73 <linus.fernandes@gmail.com>
2021-09-06 15:32:11 +0530

    Add symbolic and heights examples

---
+++ Computations/example_symbolic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sympy import cos,sin,diff,integrate,simplify,limit,solve,Symbol
+
+x = Symbol('x')
+y = Symbol('y')
+
+# Algebraic computation
+print(2*x + 3*x - y)             
+# Differentiates x**2 wrt. x
+print(diff(x**2, x))              
+# Integrates cos(x) wrt. x
+print(integrate(cos(x), x))       
+# Simplifies expression
+print(simplify((x**2 + x**3)/x**2)) 
+# Finds limit of sin(x)/x as x->0
+print(limit(sin(x)/x, x, 0))        
+# Solves 5*x = 15
+print(solve(5*x - 15, x))        
