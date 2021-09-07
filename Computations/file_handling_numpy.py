64e65747cbbda97f1b0ecc31fc3c824c80db694d
fernal73 <linus.fernandes@gmail.com>
2021-09-07 15:44:27 +0530

    Add numpy file handling

---
+++ Computations/file_handling_numpy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+filename = 'tmp.dat'
+import numpy
+data = numpy.loadtxt(filename, comments='#')
+x = data[:,0]
+y = data[:,1]
+# insert transformed y back in array
+data[:,1] = numpy.log(y)
+filename = 'tmp_out.dat'
+filename = 'tmp_out.dat'
+# open file for writing
+outfile = open(filename, 'w')  
+outfile.write('# x and y coordinates\n')
+numpy.savetxt(outfile, data, fmt='%10.5f')
