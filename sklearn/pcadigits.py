bbcd1b47e8ac6e07f005f2a239592bf0adb1d49d
fernal73 <linus.fernandes@gmail.com>
2021-09-15 17:49:08 +0530

    Add digits example

--- sklearn/pcadigits.py
+++ sklearn/pcadigits.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages('pcadigits.pdf')
@@ -20,4 +21,39 @@ plt.xlabel('component 1')
 plt.ylabel('component 2')
 plt.colorbar();
 pp.savefig()
+plt.clf()
+
+pca = PCA().fit(digits.data)
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel('number of components')
+plt.ylabel('cumulative explained variance')
+pp.savefig()
+plt.clf()
+
+def plot_digits(data):
+    fig, axes = plt.subplots(4, 10, figsize=(10, 4),
+                             subplot_kw={'xticks':[], 'yticks':[]},
+                             gridspec_kw=dict(hspace=0.1, wspace=0.1))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(data[i].reshape(8, 8),
+                  cmap='binary', interpolation='nearest',
+                  clim=(0, 16))
+plot_digits(digits.data)
+pp.savefig()
+plt.clf()
+
+np.random.seed(42)
+noisy = np.random.normal(digits.data, 4)
+plot_digits(noisy)
+pp.savefig()
+plt.clf()
+
+pca = PCA(0.50).fit(noisy)
+print(pca.n_components_)
+
+components = pca.transform(noisy)
+filtered = pca.inverse_transform(components)
+plot_digits(filtered)
+pp.savefig()
+plt.clf()
 pp.close()
