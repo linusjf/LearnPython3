5bcf945550fcde79d857fc48744a91a85f873021
fernal73 <linus.fernandes@gmail.com>
2021-09-15 16:11:30 +0530

    Add pcaspeedup2. use one dimension in pcademo

--- sklearn/pcademo.py
+++ sklearn/pcademo.py
@@ -37,4 +37,17 @@ plt.axis('equal');
 pp.savefig()
 plt.clf()
 
+pca = PCA(n_components=1)
+pca.fit(X)
+X_pca = pca.transform(X)
+print("original shape:   ", X.shape)
+print("transformed shape:", X_pca.shape)
+
+X_new = pca.inverse_transform(X_pca)
+plt.scatter(X[:, 0], X[:, 1], alpha=0.2)
+plt.scatter(X_new[:, 0], X_new[:, 1], alpha=0.8)
+plt.axis('equal');
+pp.savefig()
+plt.clf()
+
 pp.close()
