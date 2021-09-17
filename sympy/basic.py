59e975f1f03a0efeaba0d5db35937f403188e54a
fernal73 <linus.fernandes@gmail.com>
2021-09-17 09:48:04 +0000

    Add

--- sympy/basic.py
+++ sympy/basic.py
@@ -12,3 +12,22 @@ expr = expr.subs(y, x**y)
 print(expr)
 expr = expr.subs(y, x**x)
 print(expr)
+
+expr = sin(2*x) + cos(2*x)
+print(expand_trig(expr))
+print(expr.subs(sin(2*x), 2*sin(x)*cos(x)))
+
+expr = cos(x)
+print(expr.subs(x, 0))
+print(expr)
+print(x)
+expr = x**3 + 4*x*y - z
+print(expr.subs([(x, 2), (y, 4), (z, 0)]))
+expr = x**4 - 4*x**3 + 4*x**2 - 2*x + 3
+replacements = [(x**i, y**i) for i in range(5) if i % 2 == 0]
+print(replacements)
+print(expr.subs(replacements))
+expr = x**4 - 4*x**3 + 4*x**2 - 2*x + 3*y**0
+replacements = [(x**i, y**i) for i in range(5) if i % 2 == 0]
+print(replacements)
+print(expr.subs(replacements))
