c3eae48a4bc3155b70aaa21d24fbc8a2aa844e3a
fernal73 <linus.fernandes@gmail.com>
2021-09-17 13:13:19 +0530

    Add more gotchas

--- sympy/gotchas.py
+++ sympy/gotchas.py
@@ -17,3 +17,36 @@ expr = x + 1
 x = 2
 print(expr)
 
+x = 'abc'
+expr = x + 'def'
+print(expr)
+x = 'ABC'
+print(expr)
+x = symbols('x')
+expr = x + 1
+print(expr.subs(x, 2))
+
+print(x + 1 == 4)
+
+print(Eq(x + 1, 4))
+print((x + 1)**2 == x**2 + 2*x + 1)
+
+a = (x + 1)**2
+b = x**2 + 2*x + 1
+print(simplify(a - b))
+c = x**2 - 2*x + 1
+print(simplify(a - c))
+a = cos(x)**2 - sin(x)**2
+b = cos(2*x)
+print(a.equals(b))
+a = 2*sin(x)*cos(x)
+b = sin(2*x)
+print(a.equals(b))
+print(True ^ False)
+print(True ^ True)
+print(Xor(x, y))
+
+print(type(Integer(1) + 1))
+print(type(1 + 1))
+print(Integer(1)/Integer(3))
+print(type(Integer(1)/Integer(3)))
