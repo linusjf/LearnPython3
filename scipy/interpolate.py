ee027b34814f5a97aae7e22f29f2fe11909c3e51
fernal73 <linus.fernandes@gmail.com>
2021-09-22 10:00:03 +0530

    Add rbf

--- scipy/interpolate.py
+++ scipy/interpolate.py
@@ -4,6 +4,7 @@
 import numpy as np
 from scipy.interpolate import interp1d
 from scipy.interpolate import UnivariateSpline
+from scipy.interpolate import Rbf
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages('interpolate.pdf')
@@ -40,6 +41,17 @@ plt.plot(xs, spl(xs), 'g', lw = 3)
 spl.set_smoothing_factor(0.5)
 plt.plot(xs, spl(xs), 'b', lw = 3)
 plt.legend(['data', 'default', 'sf = 0.5'], loc = 'best')
+pp.savefig()
+plt.clf()
+
+
+xs = np.linspace(-3, 3, 1000)
+ys = xs**2 + np.sin(xs) + 1
+interp_func = Rbf(xs, ys)
+newx = np.arange(2, 3, 0.1)
+plt.plot(xs, ys, 'g', lw = 3)
+plt.scatter(newx, interp_func(newx),c='r')
+plt.legend(['data', 'Rbf'], loc = 'best')
 pp.savefig()
 
 pp.close()
