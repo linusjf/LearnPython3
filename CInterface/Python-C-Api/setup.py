03ccdd719b9cd764c6a215e6e509bc45c3d5049b
fernal73 <linus.fernandes@gmail.com>
2021-09-26 05:51:26 +0000

    start c interface examples

---
+++ CInterface/Python-C-Api/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup, Extension
+
+# define the extension module
+cos_module = Extension('cos_module', sources=['cos_module.c'])
+
+# run the setup
+setup(ext_modules=[cos_module])
