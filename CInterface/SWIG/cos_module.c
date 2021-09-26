6d2127f62930a4e4bf958f4e9f064d84e647dd47
fernal73 <linus.fernandes@gmail.com>
2021-09-26 15:32:44 +0530

    init swig modules

---
+++ CInterface/SWIG/cos_module.c
@@ -0,0 +1,14 @@
+/*
+ * cos_module.c
+ * Copyright (C) 2021  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "cos_module.h"
+#include <math.h>
+
+double cos_func(double arg){
+    return cos(arg);
+}
+
---
+++ CInterface/SWIG/cos_module.h
@@ -0,0 +1,13 @@
+/*
+ * cos_module.h
+ * Copyright (C) 2021  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef COS_MODULE_H
+#define COS_MODULE_H
+
+double cos_func(double arg);
+
+#endif /* !COS_MODULE_H */
