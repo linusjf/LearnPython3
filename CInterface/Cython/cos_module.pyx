ac9867e366a0a0e6d4164a8ef82a93cc75b7dcb2
fernal73 <linus.fernandes@gmail.com>
2021-09-27 05:10:02 +0530

    Add language specification

--- CInterface/Cython/cos_module.pyx
+++ CInterface/Cython/cos_module.pyx
@@ -1,7 +1,14 @@
 """ Example of wrapping cos function from math.h using Cython. """
 
-cdef extern from "math.h":
-    double cos(double arg)
+#cdef extern from "math.h":
+ #   double cos(double arg)
+
+#def cos_func(arg):
+ #   return cos(arg)
+
+""" Simpler example of wrapping cos function from math.h using Cython. """
+
+from libc.math cimport cos
 
 def cos_func(arg):
     return cos(arg)
--- CInterface/Cython/setup.py
+++ CInterface/Cython/setup.py
@@ -4,7 +4,12 @@
 from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 
+exts=[Extension("cos_module", ["cos_module.pyx"])]
+
+for e in exts:
+    e.cython_directives = {'language_level': "3"} #all are Python-3
+
 setup(
     cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("cos_module", ["cos_module.pyx"])]
+    ext_modules=exts 
 )
