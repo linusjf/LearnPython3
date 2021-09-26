0bfec71ee5a4d8a229b3b0fc7fa8bb78f920ffed
fernal73 <linus.fernandes@gmail.com>
2021-09-26 14:49:08 +0530

    Add ctype example for cos

---
+++ CInterface/Ctypes/testcosdoubles.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+import cos_doubles
+
+x = np.arange(0, 2 * np.pi, 0.1)
+y = np.empty_like(x)
+
+cos_doubles.cos_doubles_func(x, y)
+plt.plot(x, y)
+plt.savefig("testcosdoubles.png")
