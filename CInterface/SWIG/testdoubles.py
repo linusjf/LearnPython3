b5267538750e54f347422d03eb9ddfc2782fcac7
fernal73 <linus.fernandes@gmail.com>
2021-09-26 18:56:57 +0530

    Add testdoubles

---
+++ CInterface/SWIG/testdoubles.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+import cos_doubles
+
+x = np.arange(0, 2 * np.pi, 0.1)
+y = np.empty_like(x)
+
+cos_doubles.cos_doubles_func(x, y)
+plt.plot(x, y)
+plt.savefig("testdoubles.png")
+
+dir(cos_doubles)
+x = np.array([1.0,0.0,3.14159265359])
+y = np.empty_like(x)
+cos_doubles.cos_doubles_func(x,y)
+print(y)
