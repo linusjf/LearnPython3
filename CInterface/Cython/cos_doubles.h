14387ed078c7d4c5761d5f06baad75cf6f82b1c1
fernal73 <linus.fernandes@gmail.com>
2021-09-27 00:29:38 +0000

    Add numpy example

---
+++ CInterface/Cython/cos_doubles.h
@@ -0,0 +1,13 @@
+/*
+ * cos_doubles.h
+ * Copyright (C) 2021  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef COS_DOUBLES_H
+#define COS_DOUBLES_H
+#define NPY_NO_DEPRECATED_API 0
+void cos_doubles(double * in_array, double * out_array, int size);
+
+#endif /* !COS_DOUBLES_H */
