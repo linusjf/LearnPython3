44eefc093de8ea86aace12ab5d3fd07fb31a1deb
fernal73 <linus.fernandes@gmail.com>
2021-10-01 15:20:35 +0000

    add cat coding

--- patsy/readsav.py
+++ patsy/readsav.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from patsy import dmatrix
 import pandas as pd
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -10,4 +11,9 @@ df = pd.read_spss('hsb2.sav')
 print(df.tail())
 cols = ['ID', 'FEMALE', 'RACE', 'READ', 'WRITE']
 df = pd.read_spss('hsb2.sav', usecols=cols)
+
 print(df.head())
+d = dmatrix("1 + RACE",df)
+print(d)
+d = dmatrix("WRITE*RACE",df)
+print(d)
