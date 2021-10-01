3301b49b3e62739ae7c68bbd45a39a9bca33d116
fernal73 <linus.fernandes@gmail.com>
2021-10-01 23:46:35 +0000

    Add stateful

--- patsy/stateful.py
+++ patsy/stateful.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import patsy
 from patsy import dmatrix, build_design_matrices, incr_dbuilder
 data = {"x": [1, 2, 3, 4]}
 new_data = {"x": [5, 6, 7, 8]}
@@ -19,3 +20,22 @@ d = np.row_stack([build_design_matrices([dinfo], chunk)[0] for chunk in data_chu
 print(d)
 d = build_design_matrices([dinfo], new_data)[0]
 print(d)
+
+class MyExampleCenter(object):
+    def __init__(self):
+        self._total = 0
+        self._count = 0
+        self._mean = None
+
+    def memorize_chunk(self, x):
+        self._total += np.sum(x)
+        self._count += len(x)
+
+    def memorize_finish(self):
+        self._mean = self._total * 1. / self._count
+
+    def transform(self, x):
+        return x - self._mean
+
+my_example_center = patsy.stateful_transform(MyExampleCenter)
+print(my_example_center(np.array([1, 2, 3])))
