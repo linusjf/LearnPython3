29102d8f9bc48c30c4f9400f7584658bf3a55fb2
fernal73 <linus.fernandes@gmail.com>
2021-10-02 08:21:58 +0000

    Add bspline and tensors

---
+++ patsy/bspline.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from patsy import build_design_matrices,dmatrix
+
+plt.title("B-spline basis example (degree=3)");
+
+x = np.linspace(0., 1., 100)
+
+y = dmatrix("bs(x, df=6, degree=3, include_intercept=True) - 1", {"x": x})
+print("y")
+print(y.shape)
+# Define some coefficients
+b = np.array([1.3, 0.6, 0.9, 0.4, 1.6, 0.7])
+
+# Plot B-spline basis functions (colored curves) each multiplied by its coeff
+print("y*b")
+print((y*b).shape)
+plt.plot(x, y*b);
+
+# Plot the spline itself (sum of the basis functions, thick black curve)
+print("y.b")
+print(np.dot(y,b).shape)
+plt.plot(x, np.dot(y, b), color='k', linewidth=3);
+
+plt.savefig("bspline.png")
+
+data = {"x": np.linspace(0., 1., 100)}
+
+design_matrix = dmatrix("bs(x, df=4)", data)
+
+new_data = {"x": [0.1, 0.25, 0.9]}
+
+d = build_design_matrices([design_matrix.design_info], new_data)[0]
+print(d)
+
+plt.clf()
+plt.title("Natural cubic regression spline basis example");
+
+y = dmatrix("cr(x, df=6) - 1", {"x": x})
+
+# Plot natural cubic regression spline basis functions (colored curves) each multiplied by its coeff
+plt.plot(x, y*b);
+
+# Plot the spline itself (sum of the basis functions, thick black curve)
+plt.plot(x, np.dot(y, b), color='k', linewidth=3);
+plt.savefig("natural.png")
+plt.clf()
+plt.title("Cyclic cubic regression spline basis example");
+
+y = dmatrix("cc(x, df=6) - 1", {"x": x})
+
+# Plot cyclic cubic regression spline basis functions (colored curves) each multiplied by its coeff
+plt.plot(x, y*b);
+
+# Plot the spline itself (sum of the basis functions, thick black curve)
+plt.plot(x, np.dot(y, b), color='k', linewidth=3);
+plt.savefig("cyclic.png")
+
+new_design_matrix = build_design_matrices([design_matrix.design_info], new_data)[0]
+
+print(new_design_matrix)
+print(np.asarray(new_design_matrix))
