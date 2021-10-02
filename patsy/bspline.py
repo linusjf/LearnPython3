d00b4a79b562e19615716b3c0c1db1a7f2b5a7ed
fernal73 <linus.fernandes@gmail.com>
2021-10-02 14:56:56 +0000

    Add bspline randomization

--- patsy/bspline.py
+++ patsy/bspline.py
@@ -1,31 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from random import uniform
 import numpy as np
 import matplotlib.pyplot as plt
 from patsy import build_design_matrices,dmatrix
-
-plt.title("B-spline basis example (degree=3)");
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages('bspline.pdf')
+print("Setup Complete")
 
 x = np.linspace(0., 1., 100)
-
 y = dmatrix("bs(x, df=6, degree=3, include_intercept=True) - 1", {"x": x})
 print("y")
 print(y.shape)
-# Define some coefficients
-b = np.array([1.3, 0.6, 0.9, 0.4, 1.6, 0.7])
 
-# Plot B-spline basis functions (colored curves) each multiplied by its coeff
-print("y*b")
-print((y*b).shape)
-plt.plot(x, y*b);
+for i in range(3):
+    # Define some coefficients
+    #b = np.array([1.3, 0.6, 0.9, 0.4, 1.6, 0.7])
 
-# Plot the spline itself (sum of the basis functions, thick black curve)
-print("y.b")
-print(np.dot(y,b).shape)
-plt.plot(x, np.dot(y, b), color='k', linewidth=3);
+    # Define random coefficients in range 0 - 2
+    a1 = uniform(0,2)
+    a2 = uniform(0,2)
+    a3 = uniform(0,2)
+    a4 = uniform(0,2)
+    a5 = uniform(0,2)
+    a6 = uniform(0,2)
+    b = np.array([a1,a2,a3,a4,a5,a6])
+    print("b")
+    print(b)
 
-plt.savefig("bspline.png")
+    # Plot B-spline basis functions (colored curves) each multiplied by its coeff
+    print("y*b")
+    print((y*b).shape)
+    plt.title("B-spline basis example (degree=3)");
+    plt.plot(x, y*b);
+
+    # Plot the spline itself (sum of the basis functions, thick black curve)
+    print("y.b")
+    print(np.dot(y,b).shape)
+    plt.plot(x, np.dot(y, b), color='k', linewidth=3);
+    pp.savefig()
+    plt.clf()
+
+pp.close()
+#plt.savefig("bspline.png")
 
 data = {"x": np.linspace(0., 1., 100)}
 
