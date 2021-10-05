81693f3b011d33aeb2ad2bdbf4bf00cecbf0c6c8
fernal73 <linus.fernandes@gmail.com>
2021-10-05 15:33:37 +0000

    Add hello.py

---
+++ ipyparallel/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import ipyparallel as ipp
+rc = ipp.Cluster(n=4).start_and_connect_sync()
+
+#rc = cluster.connect_client_sync()
+
+rc.wait_for_engines(n=4)
+
+print(rc.ids)
+
+print(rc[:].apply_sync(lambda: "Hello, World"))
