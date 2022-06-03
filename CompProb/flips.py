736eeb7c6a0ca030dc03a692750700ede26adad6
fernal73 <linus.fernandes@gmail.com>
2022-06-03 21:47:58 +0530

    feat: start on flips

--- CompProb/flips.py
+++ CompProb/flips.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import comp_prob_inference as cpi
+from matplotlib.backends.backend_pdf import PdfPages
+
+# The PDF document
+pdf_pages = PdfPages("flips.pdf")
 
 print(cpi.flip_fair_coin())
 flips = cpi.flip_fair_coins(100)
+
 cpi.plot_discrete_histogram(flips)
-cpi.plt.savefig("flips.pdf")
+pdf_pages.savefig()
+
+cpi.plot_discrete_histogram(flips, frequency=True)
+pdf_pages.savefig()
+pdf_pages.close()
