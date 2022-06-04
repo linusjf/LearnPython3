4883510a57c4d082349ebae5b084e4c973bb3634
fernal73 <linus.fernandes@gmail.com>
2022-06-04 10:06:44 +0000

    feat: update flips example

--- CompProb/flips.py
+++ CompProb/flips.py
@@ -31,3 +31,12 @@ plt.ylabel('Fraction of heads')
 
 pdf_pages.savefig()
 pdf_pages.close()
+
+prob_space = {'sunny': 1/2, 'rainy': 1/6, 'snowy': 1/3}
+random_outcome = cpi.sample_from_finite_probability_space(prob_space)
+W = random_outcome
+if random_outcome == 'sunny':
+    I = 1
+else:
+    I = 0
+print(I)
