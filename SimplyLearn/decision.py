2b4dc961151f48d670fdba07c62998b4b3914b1f
fernal73 <linus.fernandes@gmail.com>
2022-06-21 15:54:36 +0000

    feat: start decision tree example

---
+++ SimplyLearn/decision.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+
+bal_data = pd.read_csv("dtdata.csv",sep = ",", header = 0)
