e5b20aa37e5bbcb6f7f4e4b6177f27858a51249f
fernal73 <linus.fernandes@gmail.com>
2022-06-21 16:52:31 +0000

    feat: update decision tree example

--- SimplyLearn/decision.py
+++ SimplyLearn/decision.py
@@ -8,5 +8,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import tree
 
+pd.set_option('display.max_columns', None)
 bal_data = pd.read_csv("dtdata.csv",sep = ",", header = 0)
 print("Data length: ", len(bal_data))
+print("Data shape: ", bal_data.shape)
+print("Balance data:")
+print(bal_data.head())
