447e2363921bf05570d92578fd3969728c456b74
fernal73 <linus.fernandes@gmail.com>
2022-06-23 15:55:43 +0530

    feat: update random forest example

--- SimplyLearn/randomforest.py
+++ SimplyLearn/randomforest.py
@@ -13,7 +13,16 @@ import numpy as np
 np.random.seed(0)
 
 iris = load_iris()
+#print(iris)
+#print(iris.DESCR)
 
 df = pd.DataFrame(iris.data, columns = iris.feature_names)
 
 print(df.head())
+
+df['species'] = pd.Categorical.from_codes(iris.target,iris.target_names)
+print(df.head())
+
+df['is_train'] = np.random.uniform(0,1, len(df.index)) <= 0.75
+print(df.head())
+
