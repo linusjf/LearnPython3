f5f1b93a0255093eb7c43dea6c8aa1b35d766d0d
fernal73 <linus.fernandes@gmail.com>
2022-06-25 05:40:29 +0530

    feat: start on svm examples

---
+++ SVM/vectors.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import math
+import numpy as np
+
+x = [3,4]
+# magnitude of a vector
+print(np.linalg.norm(x))
+
+# Compute the direction of a vector x.
+def direction(x):
+ return x/np.linalg.norm(x)
+
+print(direction(x))
+
+u_1 = np.array([3,4])
+u_2 = np.array([30,40])
+# [0.6 , 0.8]
+print(direction(u_1)) 
+# [0.6 , 0.8]
+print(direction(u_2))
+
+# norm of direction vector is always 1.0
+# unit vector
+print(np.linalg.norm(np.array([0.6, 0.8])))
+
+def geometric_dot_product(x,y, theta):
+    x_norm = np.linalg.norm(x)
+    y_norm = np.linalg.norm(y)
+    return x_norm * y_norm * math.cos(math.radians(theta))
+
+theta = 45 
+x = [3,5]
+y = [8,2]
+# 34.0
+print(geometric_dot_product(x,y,theta)) 
