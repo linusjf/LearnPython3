8ed18b2aa5cb81fe0cac18ababe831a9e4056614
fernal73 <linus.fernandes@gmail.com>
2022-06-25 11:56:28 +0000

    feat: start knn example

---
+++ SimplyLearn/knndiabetes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+
