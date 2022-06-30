b42bc632ad0cac30d7fbea2392143ef4d8af6dc8
fernal73 <linus.fernandes@gmail.com>
2022-06-30 19:16:01 +0530

    feat: scale cancer dataset in pca example

--- SimplyLearn/pca.py
+++ SimplyLearn/pca.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*-
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
-import pandas as pd
+import pandas as pd; pd.set_option('display.max_columns', None)
 import numpy as np
 import seaborn as sns
 
 from sklearn.datasets import load_breast_cancer
 cancer = load_breast_cancer()
 print(cancer.keys())
+print(cancer['target_names'])
+print(cancer['feature_names'])
+df = pd.DataFrame(cancer['data'],columns=cancer['feature_names'])
+print(df.head())
 
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(df)
+scaled_data = scaler.transform(df)
 # The PDF document
 pdf_pages = PdfPages("pca.pdf")
 
