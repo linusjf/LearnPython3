92c877f0ea19511c84b0d647d6cc5909d825cbdb
fernal73 <linus.fernandes@gmail.com>
2022-07-12 19:01:56 +0530

    feat: add jacobian example to CrashCalculus

---
+++ CrashCalculus/jacobian.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from sympy.abc import x, y
+from sympy import Matrix, pprint,exp,symbols
+ 
+f = Matrix([2*x*y, x**2*y])
+variables = Matrix([x,y])
+pprint(f.jacobian(variables))
+
+p,q,r,s,t,u = symbols('p q r s t u', real = True, constant = True)
+f = Matrix([1 + exp(-(p*x+q*y)),1 + exp(-(r*x+s*y)),1 + exp(-(t*x+u*y))])
+pprint(f.jacobian(variables))
