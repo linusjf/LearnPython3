6e01a2dfd06df9ec35df22457bddf72c7f893e20
fernal73 <linus.fernandes@gmail.com>
2022-07-12 13:03:48 +0530

    feat: add integral2 example to CrashCalculus

--- CrashCalculus/integral2.py
+++ CrashCalculus/integral2.py
@@ -3,11 +3,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-pp = PdfPages('integral.pdf')
+pp = PdfPages('integral2.pdf')
 print("Setup Complete")
 
 def f(x):
-    return 2*x
+    return 3*x**2 - 4*x
  
 # Set up x from -10 to 10 with small steps
 delta_x = 0.1
@@ -15,9 +15,12 @@ x = np.arange(-10, 10, delta_x)
 # Find f(x) * delta_x
 fx = f(x) * delta_x
 # Compute the running sum
-y = fx.cumsum()
+y1 = fx.cumsum()
+y2 = x**3 - 2*x**2
 # Plot
-plt.plot(x, y)
-plt.title("Integral of 2x")
+plt.plot(x, y1, c="r", alpha=0.5, label="area")
+plt.plot(x, y2, c="b", alpha=0.5, label="antiderivative")
+plt.title("Integral of 3x² - 4x")
+plt.legend()
 pp.savefig()
 pp.close()
