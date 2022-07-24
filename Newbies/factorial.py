d0641d425053b001586f57c1743715feb3ba3acc
fernal73 <linus.fernandes@gmail.com>
2022-07-24 21:25:22 +0530

    feat: add combination example

--- Newbies/factorial.py
+++ Newbies/factorial.py
@@ -4,3 +4,4 @@ import math
 print("20! = {}".format(math.factorial(20)))
 print("100! = {}".format(math.factorial(100)))
 print("²⁰P₁₀ = {}".format(math.factorial(20)/math.factorial(10)))
+print("²⁰C₁₀ = {}".format(math.factorial(20)/(math.factorial(10)*math.factorial(10))))
