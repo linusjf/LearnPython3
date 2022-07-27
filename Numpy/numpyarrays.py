5bc6d6f4c109f62c8145ec27236194cc27570a75
fernal73 <linus.fernandes@gmail.com>
2022-07-27 07:05:59 +0000

    feat: update numpy arrays example

--- Numpy/numpyarrays.py
+++ Numpy/numpyarrays.py
@@ -8,3 +8,13 @@ B = numpy.array([[1, 1, 2], [8, 4, 1], [0, 0, 3]])
 print(A[0,0])
 # print the entry in the second row and third column of B
 print(B[1,2])
+# Add A and B
+print(numpy.add(A,B))
+# Subtract A and B
+print(numpy.subtract(A,B))
+# Multiply A by a scalar 5
+print(numpy.multiply(5,A))
+# Find the transpose of A
+print(numpy.transpose(A))
+# Multiply A and B
+print(numpy.dot(A,B))
