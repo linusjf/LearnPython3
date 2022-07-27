f01eaab3028fdc9052995ab03b34befc5d6f3bde
fernal73 <linus.fernandes@gmail.com>
2022-07-27 11:46:53 +0530

    feat: add numpy arrays example

---
+++ Numpy/numpyarrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy 
+# initialize matrices 
+A = numpy.array([[3, 2, 1], [9, 0, 1], [3, 4, 1]]) 
+B = numpy.array([[1, 1, 2], [8, 4, 1], [0, 0, 3]])
+# print the entry in the first row and first column of A
+print(A[0,0])
+# print the entry in the second row and third column of B
+print(B[1,2])
