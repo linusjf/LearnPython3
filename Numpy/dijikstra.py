5bcecf96fde251417e0351ece42711ca3e23dca1
fernal73 <linus.fernandes@gmail.com>
2022-08-06 09:51:41 +0530

    feat: add disconnected networks example

--- Numpy/dijikstra.py
+++ Numpy/dijikstra.py
@@ -103,6 +103,52 @@ def Dijkstra(W, i):
     # return the outputs
     return shortestDistances, previousVertices
 
+# find the shortest paths to connected vertices
+def distancesWithinComponent(source):
+    # initialize the connected component
+    component = [source]
+    # construct the connected component
+    for i in range(1, W2.shape[0] + 1):
+        if i != source and isConnected(W2, source, i):
+            component.append(i)
+        
+    # find the weight matrix correponding to the connected component
+    subnetwork = W2[numpy.array(component) - 1,:][:,numpy.array(component) - 1]
+    # run Dijkstra's algorithm
+    return Dijkstra(subnetwork, 1)
+
+# create a function that returns True if vertex i and vertex j 
+# are connected in the graph represented by the input
+# adjacency matrix A
+def isConnected(A, i, j):
+    # initialize the paths matrix to adjacency matrix A
+    paths = A
+
+    # find the number of vertices in the graph
+    numberOfVertices = A.shape[0]
+
+    # find the number of edges in the graph
+    numberOfEdges = numpy.sum(A)/2
+    # if vi and vj are adjacent, return True
+    if paths[i-1][j-1] > 0:
+        print('Vertex', i, 'and vertex', j, 'are adjacent')
+        return True
+    else:
+        # run the loop until we find a path
+        for pathLength in range(2, numberOfVertices):
+            # exponentiate the adjacency matrix
+            paths = numpy.dot(paths, A)
+            # if the element in row i, column j is more than 0, 
+            # we found a path
+            if paths[i-1][j-1] > 0:
+                print('There is a path with', pathLength,'edges from vertex', i, 'to vertex', j)
+                return True
+
+    # found no paths, the vertices are not connected
+    if pathLength == numberOfEdges:
+        print('There are no paths from vertex', i, 'to vertex', j)
+        return False
+
 # Create a weight matrix for the network in Figure 9.15
 W1 = numpy.array([[0, 4, 1, 0, 2, 0],
  [4, 0, 2, 1, 0, 1],
@@ -116,3 +162,14 @@ s,p = Dijkstra(W1, 1)
 print(s,p)
 for i in range(2,7):
     printShortestPath(s, p, 1, i)
+
+# Create a weight matrix for the network in Figure 9.16
+W2 = numpy.array([[0, 4, 0, 0, 0, 0],
+ [4, 0, 0, 0, 0, 1],
+ [0, 0, 0, 1, 4, 0],
+ [0, 0, 1, 0, 2, 0],
+ [0, 0, 4, 2, 0, 0],
+ [0, 1, 0, 0, 0, 0]])
+
+distancesWithinComponent(1)
+distancesWithinComponent(3)
