19fd0ca33f5a342ba5dd0cda0b8409d5d2cb241b
fernal73 <linus.fernandes@gmail.com>
2022-08-07 14:24:14 +0530

    feat: add scatterplot to linear regression example

--- discrete/regression.py
+++ discrete/regression.py
@@ -14,7 +14,7 @@ df = origdf[origdf.horsepower != "?"]
 # Plotting the pairplot
 sns.pairplot(df, diag_kind="kde")
 pp.savefig()
-pp.close()
+plt.clf()
 test = origdf[origdf.horsepower == "?"]
 print(df.shape)
 print(test.shape)
@@ -30,3 +30,12 @@ print("Weights of cars: ",X_new)
 X_new_b = np.c_[np.ones((test.shape[0],1)),X_new]
 y_predict = X_new_b.dot(beta_values)
 print("Predicted horsepower of cars: ",y_predict)
+X_plot= np.array([[1500],[6000]])
+X_plot_b = np.c_[np.ones((2,1)),X_plot] 
+Y_plot = X_plot_b.dot(beta_values)
+Equationline = "Y ={:.3f}+{:.3f}X".format(beta_values[0], beta_values[1])
+plt.plot(X_plot, Y_plot, "r-", label = Equationline)
+sns.scatterplot(x=X,y=Y, label = "Training Data")
+plt.legend()
+pp.savefig()
+pp.close()
