fb5997df312f5e71a408496d8516f6500671f023
fernal73 <linus.fernandes@gmail.com>
2022-08-08 04:58:48 +0530

    feat: add 3dplot for multivariate linear regression

--- discrete/regression.py
+++ discrete/regression.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 import seaborn as sns
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages('regression.pdf')
 print("Setup Complete")
@@ -77,5 +78,37 @@ Equationline = "Y ={:.4f} $X^2$ {:.3f} $X$ + {:.3f}".format(reg.coef_[0,1], reg.
 sns.scatterplot(x=X,y=y, label = "Training Data")
 plt.plot(X_plot, Y_plot, "r-", label = Equationline)
 plt.legend()
+pp.savefig()
+
+Y = df["displacement"]
+X = df[["horsepower","weight"]]
+#Fitting the linear regression model
+reg = LinearRegression()
+reg.fit(X, Y)
+# Printing the parameter values obtained after fitting the model
+print("The value obtained for beta_o is: ", reg.intercept_)
+print("The value obtained for beta_1 and beta_2 are: ",reg.coef_[0] , "and", reg.coef_[1] )
+
+# Plotting the surface plot
+X1_min = df["horsepower"].values.min()
+X1_max = df["horsepower"].values.max()
+X1_values = np.linspace(X1_min, X1_max, 100)
+X2_min = df["weight"].values.min()
+X2_max = df["weight"].values.max()
+X2_values = np.linspace(X2_min, X2_max, 100)
+Y_reg = reg.intercept_ + (reg.coef_[0] * X1_values) + (reg.
+ coef_[1] * X2_values)
+Y_plot = Y_reg.reshape(-1,1)
+fig = plt.figure(figsize=(11.69,8.27))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X.horsepower, X.weight, Y, color="red", s=1, label="Training data")
+X1_plot, X2_plot = np.meshgrid(X1_values, X2_values)
+surf = ax.plot_wireframe(X1_plot, X2_plot, Y_plot, rstride=10, 
+ cstride=10)
+ax.view_init(50, 150)
+ax.set_xlabel('Horsepower')
+ax.set_ylabel('Weight')
+ax.set_zlabel('Displacement')
+ax.legend()
 pp.savefig()
 pp.close()
