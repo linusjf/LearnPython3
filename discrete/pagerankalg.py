38019c93a8541562097c036f529c2eb4495c1d2c
fernal73 <linus.fernandes@gmail.com>
2022-08-15 12:59:01 +0530

    feat: add California.txt

--- discrete/pagerankalg.py
+++ discrete/pagerankalg.py
@@ -61,3 +61,12 @@ A = numpy.array([[0, 1/4, 1/4, 1/4, 1/4],
  [0, 0, 0, 1, 0]])
 # Run the PageRank algorithm with default settings
 print(PageRank(A, verbose=True))
+
+# transition probability matrix
+B = numpy.array([[0, 1/4, 1/4, 1/4, 1/4],
+ [1/3, 0, 1/3, 1/3, 0],
+ [1/3, 0, 0, 1/3, 1/3],
+ [1/2, 0, 1/2, 0, 0],
+ [0, 0, 1/2, 1/2, 0]])
+# Run the PageRank algorithm with default settings
+print(PageRank(B, verbose = True))
