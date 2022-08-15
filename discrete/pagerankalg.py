f0e77fd0640054e71ff9e959894cec7f001444d5
fernal73 <linus.fernandes@gmail.com>
2022-08-15 09:18:55 +0000

    feat: run pagerank on California.txt

--- discrete/pagerankalg.py
+++ discrete/pagerankalg.py
@@ -70,3 +70,46 @@ B = numpy.array([[0, 1/4, 1/4, 1/4, 1/4],
  [0, 0, 1/2, 1/2, 0]])
 # Run the PageRank algorithm with default settings
 print(PageRank(B, verbose = True))
+
+# import the pandas library
+import pandas
+# read the txt file into a dataframe
+data = pandas.read_csv("California.txt", delimiter=' ')
+# display the dataframe
+print(data)
+
+# preprocess the data
+# select only the rows with type 'e'
+adjacencies = data.loc[data['Type'] == 'e']
+# drop the 'Type' column
+adjacencies = adjacencies.drop(columns = 'Type')
+# convert the adjacency list to a NumPy array
+adjacencies = adjacencies.to_numpy()
+
+# convert the adjacency list to integers
+adjacencies = adjacencies.astype('int')
+# print the adjacency list
+print(adjacencies)
+
+# convert the adjacency list to an adjacency matrix
+# find the number of webpages and initialize A
+N = numpy.max(adjacencies) + 1
+A = numpy.zeros([N, N])
+# iterate over the rows of the adjacency list
+for k in range(adjacencies.shape[0]):
+    # find the adjacent vertex numbers
+    i, j = adjacencies[k,]
+
+    # put 1 in the adjacency matrix
+    A[i, j] = 1
+
+# convert A to the transition probability matrix
+# divide each row of A by its row sum
+rowSums = A.sum(axis = 1)[:,None]
+# divide A by the rowSums
+A = numpy.divide(A, rowSums, where = rowSums != 0)
+# run PageRank
+v, i = PageRank(A,verbose=True)
+# print the steady state PageRank vector and iteration number
+print(v)
+print(i)
