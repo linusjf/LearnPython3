c61980c1f9c23a6d7529c3a40690ab0ced2edb16
fernal73 <linus.fernandes@gmail.com>
2022-08-17 21:51:00 +0530

    feat: add data structure examples

--- NetworkX/graphcreation.py
+++ NetworkX/graphcreation.py
@@ -34,3 +34,13 @@ subax2 = plt.subplot(122)
 nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
 pp.savefig()
 pp.close()
+
+G = nx.Graph()
+G.add_edge('A', 'B')
+G.add_edge('B', 'C')
+print(G.adj)
+
+G = nx.Graph()
+G.add_edge(1, 2, color='red', weight=0.84, size=300)
+print(G[1][2]['size'])
+print(G.edges[1, 2]['color'])
