fef54304f0bac6d5a2fcaaad9baad82c7b82cf86
fernal73 <linus.fernandes@gmail.com>
2022-08-20 22:02:45 +0530

    feat: add digraph and multidigraph examples

--- NetworkX/graphtypes.py
+++ NetworkX/graphtypes.py
@@ -226,3 +226,75 @@ e = (2, 3, {"weight": 7})
 # select first part of edge tuple
 G.remove_edge(*e[:2])
 printgraph(G)
+
+# or DiGraph, MultiGraph, MultiDiGraph, etc
+G = nx.path_graph(4) 
+printgraph(G)
+ebunch = [(1, 2), (2, 3)]
+G.remove_edges_from(ebunch)
+printgraph(G)
+
+# dict-of-set/list/tuple
+adj = {1: {2, 3}, 2: {1, 3}, 3: {1, 2}}
+e = [(u, v) for u, nbrs in adj.items() for v in nbrs]
+G.update(edges=e, nodes=adj)
+printgraph(G)
+DG = nx.DiGraph()
+printgraph(DG)
+# dict-of-dict-of-attribute
+adj = {1: {2: 1.3, 3: 0.7}, 2: {1: 1.4}, 3: {1: 0.7}}
+e = [
+    (u, v, {"weight": d})
+    for u, nbrs in adj.items()
+    for v, d in nbrs.items()
+]
+DG.update(edges=e, nodes=adj)
+printgraph(DG)
+# dict-of-dict-of-dict
+adj = {1: {2: {"weight": 1.3}, 3: {"color": 0.7, "weight": 1.2}}}
+e = [
+    (u, v, {"weight": d})
+    for u, nbrs in adj.items()
+    for v, d in nbrs.items()
+]
+DG.update(edges=e, nodes=adj)
+printgraph(DG)
+# predecessor adjacency (dict-of-set)
+pred = {1: {2, 3}, 2: {3}, 3: {3}}
+e = [(v, u) 
+     for u, nbrs in pred.items() 
+     for v in nbrs
+     ]
+print(e)
+
+# MultiGraph dict-of-dict-of-dict-of-attribute
+MDG = nx.MultiDiGraph()
+adj = {
+    1: {2: {0: {"weight": 1.3}, 1: {"weight": 1.2}}},
+    3: {2: {0: {"weight": 0.7}}},
+}
+e = [
+    (u, v, ekey, d)
+    for u, nbrs in adj.items()
+    for v, keydict in nbrs.items()
+    for ekey, d in keydict.items()
+]
+print(e)
+printgraph(MDG)
+MDG.update(edges=e)
+printgraph(MDG)
+G = nx.path_graph(5)
+printgraph(G)
+G.update(nx.complete_graph(range(4, 10)))
+printgraph(G)
+from itertools import combinations
+edges = (
+    (u, v, {"power": u * v})
+    for u, v in combinations(range(10, 20), 2)
+    if u * v < 225
+)
+print(edges)
+# for singleton, use a container
+nodes = [1000] 
+G.update(edges, nodes)
+printgraph(G)
