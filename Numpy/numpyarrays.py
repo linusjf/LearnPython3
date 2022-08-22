a9a210634b488b649cb30642748b9528eed20597
fernal73 <linus.fernandes@gmail.com>
2022-08-22 20:04:08 +0530

    feat: add vstack and hstack examples

--- Numpy/numpyarrays.py
+++ Numpy/numpyarrays.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy 
+from numpy import array
+from numpy import vstack
+from numpy import hstack
 # initialize matrices 
 A = numpy.array([[3, 2, 1], [9, 0, 1], [3, 4, 1]]) 
 B = numpy.array([[1, 1, 2], [8, 4, 1], [0, 0, 3]])
@@ -18,3 +21,27 @@ print(numpy.multiply(5,A))
 print(numpy.transpose(A))
 # Multiply A and B
 print(numpy.dot(A,B))
+
+# create array with vstack
+# create first array
+a1 = array([1,2,3])
+print(a1)
+# create second array
+a2 = array([4,5,6])
+print(a2)
+# vertical stack
+a3 = vstack((a1, a2))
+print(a3)
+print(a3.shape)
+
+# create array with hstack
+# create first array
+a1 = array([1,2,3])
+print(a1)
+# create second array
+a2 = array([4,5,6])
+print(a2)
+# create horizontal stack
+a3 = hstack((a1, a2))
+print(a3)
+print(a3.shape)
