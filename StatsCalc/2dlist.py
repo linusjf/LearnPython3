0edb0e412e617f2bddda83548169f24aec59f289
fernal73 <linus.fernandes@gmail.com>
2022-08-25 16:10:33 +0530

    feat: start stats calculus workshop

---
+++ StatsCalc/2dlist.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for row in a:
+    print(row[0])
+for row in a:
+    for element in row:
+        print(element)
+for i in range(3):
+    print(a[i][i])
+for i in range(3):
+    print(f'The {i + 1}-th diagonal element is: {a[i][i]}')
