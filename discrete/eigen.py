98d3516de2770f48701a855b07f4889c69520b5f
fernal73 <linus.fernandes@gmail.com>
2022-08-25 10:22:43 +0530

    feat: add eigen and pca scripts

---
+++ discrete/eigen.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+A = np.array([[3,1], [1,3]])
+l, v = np.linalg.eig(A)
+print("The eigenvalues are:\n ",l)
+print("The eigenvectors are:\n ", v)
