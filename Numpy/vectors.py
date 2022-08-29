b2dc87a4be13de7fe452ca8bee8035d56b9a8427
fernal73 <linus.fernandes@gmail.com>
2022-08-29 11:24:38 +0530

    feat: add vectors examples

---
+++ Numpy/vectors.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# create a vector
+from numpy import array
+# define vector
+v = array([1, 2, 3])
+print(v)
+
+# define first vector
+a = array([1, 2, 3])
+print(a)
+# define second vector
+b = array([1, 2, 3])
+print(b)
+# add vectors
+c = a + b
+print(c)
+
+# define first vector
+a = array([1, 2, 3])
+print(a)
+# define second vector
+b = array([0.5, 0.5, 0.5])
+print(b)
+# subtract vectors
+c = a - b
+print(c)
+
+# define first vector
+a = array([1, 2, 3])
+print(a)
+# define second vector
+b = array([1, 2, 3])
+print(b)
+# multiply vectors
+c = a * b
+print(c)
+
+# define first vector
+a = array([1, 2, 3])
+print(a)
+# define second vector
+b = array([1, 2, 3])
+print(b)
+# divide vectors
+c = a / b
+print(c)
+
+# define first vector
+a = array([1, 2, 3])
+print(a)
+# define second vector
+b = array([1, 2, 3])
+print(b)
+# multiply vectors
+c = a.dot(b)
+print(c)
+
+# define vector
+a = array([1, 2, 3])
+print(a)
+# define scalar
+s = 0.5
+print(s)
+# multiplication
+c = s * a
+print(c)
