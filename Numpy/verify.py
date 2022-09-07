25d94bd0a7b99718ad13e1d59f8f9afad4f0be1f
fernal73 <linus.fernandes@gmail.com>
2022-09-07 09:11:31 +0530

    feat: refactor example to call function verify instead

--- Numpy/verify.py
+++ Numpy/verify.py
@@ -9,16 +9,28 @@ from numpy import prod
 from numpy import sum
 from numpy.linalg import eigvals
 from numpy.linalg import det
+
+def verify(A):
+    print(A)
+    # factorize
+    values = eigvals(A)
+    print(values)
+    print(det(A))
+    print(prod(values))
+    print(trace(A))
+    print(sum(values))
+    print()
+
+
 # define matrix
 A = array([
 [1, 2, 3],
 [4, 5, 6],
 [7, 8, 9]])
-print(A)
-# factorize
-values = eigvals(A)
-print(values)
-print(det(A))
-print(prod(values))
-print(trace(A))
-print(sum(values))
+verify(A)
+
+A = array([
+    [2,-1,0],
+    [-1,2,-1],
+    [0,-1,2]])
+verify(A)
