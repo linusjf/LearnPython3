814188ce25d4e5de9baea0fd5cb014b64c0d4a63
Linus Fernandes <linus.fernandes@gmail.com>
2022-12-12 15:50:22 +0530

    feat: add avatar

--- AutomateIt/avatar.py
+++ AutomateIt/avatar.py
@@ -13,3 +13,9 @@ soup = BeautifulSoup(req.urlopen(req.Request(url,headers=header)),"lxml")
 images = [a['src'] for a in soup.find_all("img", {"src":re.compile("gstatic.com")})][:5]
 for img in images:
     print("Image Source:", img)
+for img in images:
+    raw_img = req.urlopen(img).read()
+    cntr = len([i for i in os.listdir(".") if image_type in i]) + 1
+    f = open(image_type + "_"+ str(cntr)+".jpg", 'wb')
+    f.write(raw_img)
+    f.close()
