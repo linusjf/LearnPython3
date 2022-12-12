5f587872a020b674785664a0669ec9f9c1b7eb2b
Linus Fernandes <linus.fernandes@gmail.com>
2022-12-13 04:40:15 +0530

    feat: add gists

---
+++ AutomateIt/gists.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+token = ""
+try:
+    with open('.gisttoken','r') as file:
+        token = file.read()
+except Exception as exc:
+    print(exc)
+    exit(1)
+
+import requests
+import json
+ 
+BASE_URL = 'https://api.github.com'
+Link_URL = 'https://gist.github.com'
+## Fill in your github username
+username = 'fernal73' 
+## Fill in your token
+api_token = token.rstrip() 
+header = {
+    'X-Github-Username': '%s' % username,
+    'Content-Type': 'application/json',
+    'Authorization': 'Token %s' % api_token}
+print(header)
+url = "/gists"
+data = {
+    "description": "the description for this gist",
+    "public": True,
+    "files": {
+        "file1.txt": {
+            "content": "String file contents"
+        }
+    }
+}
+print(data)
+gisturl = ""
+try:
+    r = requests.post('%s%s' % (BASE_URL, url),
+                  headers=header,
+                  data=json.dumps(data))
+    gisturl = r.json()['url']
+    print(gisturl)
+except Exception as exc:
+    print(exc)
+    exit(1)
+
+try:
+    r = requests.get('%s' %gisturl,
+                     headers=header)
+    print(r.json())
+except Exception as exc:
+    print(exc)
+    exit(1)
+
+data = { 
+    "description": "Updating the description for this gist",
+    "files": {
+        "file1.txt": {
+            "content": "Updating file contents.."
+        }
+    }
+}
+try:
+    r = requests.patch('%s' %gisturl,
+                       headers=header,
+                       data=json.dumps(data))
+    print(r.json())
+except Exception as exc:
+    print(exc)
+    exit(1)
+
+try:
+    r = requests.delete('%s' %gisturl,
+                    headers=header)
+except Exception as exc:
+    print(exc)
+    exit(1)
