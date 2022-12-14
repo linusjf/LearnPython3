b55dc8daeb70a2f09680e0b7096a7a41c8ec9d3f
Linus Fernandes <linus.fernandes@gmail.com>
2022-12-14 20:15:15 +0530

    feat: add readcsv

---
+++ AutomateIt/readcsv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import csv
+fh = open("mylist.csv", 'rt')
+try:
+    reader = csv.reader(fh)
+    print("Data from the CSV:")
+    print(list(reader))
+except Exception as e:
+    print("Exception is:", e)
+finally:
+    fh.close()
