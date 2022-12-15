50dbfa4b75ad3dc29dd396255eaf2ec7074dd459
Linus Fernandes <linus.fernandes@gmail.com>
2022-12-15 11:46:17 +0530

    feat: add readdict

---
+++ AutomateIt/readdict.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import csv
+f = open("mylist.csv", 'rt')
+print("File Contents:")
+try:
+    reader = csv.DictReader(f)
+    for row in reader:
+        print(row['first_name'], row['last_name'], row['email'])
+finally:
+    f.close()
+
+f = open("mylist.csv", 'rt')
+reader = csv.DictReader(f)
+print("Columns in CSV file:", reader.fieldnames)
+print("Dialect used in CSV file:", reader.dialect)
+print("Current line number in CSV file:", reader.line_num)
+print("Moving the reader to next line with reader.__next__()")
+reader.__next__()
+print("Reading line number:", reader.line_num )
+f.close()
