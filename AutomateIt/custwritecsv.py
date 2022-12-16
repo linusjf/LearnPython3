9e7943e1504120477429d22877d1c723723c04fb
Linus Fernandes <linus.fernandes@gmail.com>
2022-12-16 09:10:01 +0530

    feat: add custwritecsv

---
+++ AutomateIt/custwritecsv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import csv
+f=open("write.csv", 'wt')
+csvWriter = csv.writer(f, delimiter='\t',lineterminator='\n\n')
+csvWriter.writerow(['abc', 'pqr', 'xyz'])
+csvWriter.writerow(['123', '456', '789'])
+f.close()
