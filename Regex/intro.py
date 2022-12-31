7f99839f4b4f67fce5a6d13a3cd6d909b70aee3c
Linus Fernandes <linus.fernandes@gmail.com>
2022-12-31 14:17:42 +0530

    feat: add more regex examples

--- Regex/intro.py
+++ Regex/intro.py
@@ -9,3 +9,12 @@ import re
 # check if 'sentence' contains the pattern described by RE argument
 print(bool(re.search(r'is', sentence)))
 print(bool(re.search(r'xyz', sentence)))
+
+if re.search(r'ring', sentence):
+    print('mission success')
+if not re.search(r'xyz', sentence):
+    print('mission failed')
+words = ['cat', 'attempt', 'tattle']
+print([w for w in words if re.search(r'tt', w)])
+print(all(re.search(r'at', w) for w in words))
+print(any(re.search(r'stat', w) for w in words))
