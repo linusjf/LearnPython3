14a09fa6e730b3d1dd035a47fc02bbf124cd8f84
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-03 12:34:01 +0530

    feat: add CAEmployees example

---
+++ AutomateIt/CAEmployees.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import csv
+f = open("mylist.csv", 'rt')
+fw = open("CAEmployees.csv", 'wt')
+try:
+    reader = csv.DictReader(f)
+    csvWriter = csv.writer(fw)
+    for row in reader:
+        if reader.line_num == 1:
+            continue
+        if row['state'] == 'CA':
+            csvWriter.writerow([row['email'], row['phone']])
+finally:
+    f.close()
+    fw.close()
