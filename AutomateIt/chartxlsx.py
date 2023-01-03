d6183ed6f2596d73d2cfc68b429540c9e405ee7e
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-04 05:09:56 +0530

    feat: add chart example

---
+++ AutomateIt/chartxlsx.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xlsxwriter
+workbook = xlsxwriter.Workbook('chart_line.xlsx')
+worksheet = workbook.add_worksheet()
+data = [10, 40, 50, 20, 10, 50]
+worksheet.write_column('A1', data)
+chart = workbook.add_chart({'type': 'line'})
+chart.add_series({'values': '=Sheet1!$A$1:$A$6'})
+worksheet.insert_chart('C1', chart)
+workbook.close()
