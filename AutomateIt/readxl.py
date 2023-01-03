f26c8dc31ea0f2ab8c1a203ebe64c48f4ed459c6
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-03 14:22:14 +0530

    feat: add readxl example

---
+++ AutomateIt/readxl.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import openpyxl
+workbook = openpyxl.load_workbook('myxlsx.xlsx')
+print("Workbook Object:", workbook.sheetnames)
+people = workbook['People']
+print("People sheet object:", people)
+print("First cell Object:", people['A1'])
+print("Other Cell Object:", people.cell(row=3, column=2))
+print("First Name:", people['B2'].value,people['C2'].value)
