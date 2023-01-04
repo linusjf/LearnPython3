f84b4a01d446656dfcbff79538ff927f60ddec47
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-04 14:32:17 +0530

    feat: add pdfcopy example

---
+++ AutomateIt/pdfcopy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from PyPDF2 import PdfReader, PdfWriter
+infile = PdfReader(open('diveintopython.pdf', 'rb'))
+outfile = PdfWriter()
+outfile.add_blank_page(612, 792)
+p = infile.pages[0]
+outfile.add_page(p)
+with open('myPdf.pdf', 'wb') as f:
+    outfile.write(f)
+f.close()
