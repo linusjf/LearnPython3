38aa177cba1b48f670adbf67ea71ecb633aeb54c
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-05 19:36:08 +0530

    feat: add pdfencrypt example

---
+++ AutomateIt/pdfencrypt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from PyPDF2 import PdfWriter, PdfReader
+fp = open('diveintopython.pdf', 'rb')
+readerObj = PdfReader(fp)
+writer = PdfWriter()
+for page in range(len(readerObj.pages)):
+ writer.add_page(readerObj.pages[page])
+writer.encrypt('P@$$w0rd')
+newfp = open('EncryptExercise.pdf', 'wb')
+writer.write(newfp)
+newfp.close()
+fp.close()
