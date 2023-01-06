9b93178e1585f649b09a2e75e89dae29ec93fea5
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-06 22:37:49 +0530

    feat: add mimemail example

---
+++ AutomateIt/mimemail.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import config
+import email.utils
+fromaddr = config.fromaddr
+toaddr = config.toaddr
+msg = MIMEMultipart()
+msg['Subject'] = "Hello from the Reader of Automate It!"
+msg['To'] = email.utils.formataddr(('Recipient', toaddr))
+msg['From'] = email.utils.formataddr(('Author',fromaddr))
+body = "What a wonderful world!"
+msgBody = MIMEText(body, 'plain')
+msg.attach(msgBody)
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, config.password)
+text = msg.as_string()
+print("Text is:", text)
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
