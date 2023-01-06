4ade7f01d00f30056d66a46d402c0846162a8f6b
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-06 06:04:53 +0530

    feat: add docread example

---
+++ AutomateIt/docread.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import docx
+doc = docx.Document('WExercise.docx')
+print("Document Object:", doc)
+print("Title of the document:")
+print(doc.paragraphs[0].text)
+print("Attributes of the document")
+print("Author:", doc.core_properties.author)
+print("Date Created:", doc.core_properties.created)
+print("Document Revision:", doc.core_properties.revision)
+
+if (doc.tables):
+    table = doc.tables[0]
+    print("Column 1:")
+    for i in range(len(table.rows)):
+        print(table.rows[i].cells[0].paragraphs[0].text)
+    print("Column 2:")
+    for i in range(len(table.rows)):
+        print(table.rows[i].cells[1].paragraphs[0].text)
+    print("Column 3:")
+    for i in range(len(table.rows)):
+        print(table.rows[i].cells[2].paragraphs[0].text)
