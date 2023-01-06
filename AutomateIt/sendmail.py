ac9f7ece8e238ad4d8f20b5fcda38efb19493150
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-06 22:09:54 +0530

    feat: add sendmail example

---
+++ AutomateIt/sendmail.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import smtplib
+import config 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(config.fromaddr, config.password)
+msg = "Some nice msg"
+server.sendmail(config.fromaddr, config.toaddr, msg)
+server.quit()
