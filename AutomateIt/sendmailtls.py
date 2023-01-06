297ee7c85be60868c2850bf3aa0e129be497d0e3
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-06 22:37:49 +0530

    feat: add mimemail example

---
+++ AutomateIt/sendmailtls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com', 587)
+try:
+    server.set_debuglevel(True)
+    print("Sending ehlo")
+    server.ehlo()
+    if server.has_extn('STARTTLS'):
+        print("Starting TLS Session")
+        server.starttls()
+        print("Sending ehlo again")
+        server.ehlo()
+finally:
+     server.quit()
