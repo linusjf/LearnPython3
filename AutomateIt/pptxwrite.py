584cb13dfa583df1bd5725de7e928bedb71a2215
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-08 12:37:25 +0530

    feat: add pptxwrite example fix: update cpdocs script to create pydocs dir

---
+++ AutomateIt/pptxwrite.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : pptxwrite
+# @created     : Sunday Jan 08, 2023 12:31:53 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+import collections
+import collections.abc
+from pptx import Presentation
+
+prs = Presentation()
+slide = prs.slides.add_slide(prs.slide_layouts[0])
+slide.shapes.title.text = "Yo, Python!"
+slide.placeholders[1].text = "Yes it is really awesome"
+prs.save('yoPython.pptx')
