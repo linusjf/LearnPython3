d7faa665a7a23cee2afddad2fca5139a47d87f90
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-09 13:57:16 +0530

    feat: add ppt two content

---
+++ AutomateIt/pptxtwocontent.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : pptxtwocontent
+# @created     : Monday Jan 09, 2023 13:29:52 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+import collections
+import collections.abc
+
+from pptx import Presentation
+
+prs = Presentation()
+two_content_slide_layout = prs.slide_layouts[3]
+slide = prs.slides.add_slide(two_content_slide_layout)
+shapes = slide.shapes
+title_shape = shapes.title
+title_shape.text = 'Adding a Two Content Slide'
+body_shape = shapes.placeholders[1]
+tf = body_shape.text_frame
+tf.text = 'This is line 1.'
+p = tf.add_paragraph()
+p.text = 'Again a Line 2..'
+p.level = 1
+p = tf.add_paragraph()
+p.text = 'And this is line 3...'
+p.level = 2
+prs.save('twocontent.pptx')
