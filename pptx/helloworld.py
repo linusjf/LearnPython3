28ab6497bade22f5d06e743a083a47012160150f
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-09 15:55:52 +0530

    feat: add helloworld.py to pptx

---
+++ pptx/helloworld.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : helloworld
+# @created     : Monday Jan 09, 2023 15:52:17 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+import collections
+import collections.abc
+from pptx import Presentation
+
+prs = Presentation()
+title_slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(title_slide_layout)
+title = slide.shapes.title
+subtitle = slide.placeholders[1]
+
+title.text = "Hello, World!"
+subtitle.text = "python-pptx was here!"
+
+prs.save('test.pptx')
