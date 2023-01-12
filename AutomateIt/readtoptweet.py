8f67d0467276a708a64b4561c4748394ccca8c6e
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-12 18:08:23 +0530

    feat: add readtoptweet which doesn't work w/o elevated access

--- AutomateIt/readtoptweet.py
+++ AutomateIt/readtoptweet.py
@@ -2,25 +2,20 @@
 """
 ######################################################################
 # @author      : Linus Fernandes (linusfernandes at gmail dot com)
-# @file        : createtweet
-# @created     : Thursday Jan 12, 2023 16:07:44 IST
+# @file        : readtoptweet
+# @created     : Thursday Jan 12, 2023 17:25:11 IST
 # @description :
 # -*- coding: utf-8 -*-'
 ######################################################################
 """
-import json
-
 from requests_oauthlib import OAuth1Session
+import twython
+from twython import Twython
 
 import config
 
 consumer_key = config.twitterapikey
 consumer_secret = config.twitterapikeysecret
-
-# Be sure to add replace the text with the text you wish to Tweet.
-# You can also add parameters to post polls, quote Tweets, Tweet with reply settings,
-# and Tweet to Super Followers in addition to other features.
-payload = {"text": "Hello world!"}
 
 # Get request token
 REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
@@ -56,27 +51,13 @@ oauth_tokens = oauth.fetch_access_token(ACCESS_TOKEN_URL)
 
 access_token = oauth_tokens["oauth_token"]
 access_token_secret = oauth_tokens["oauth_token_secret"]
-
-# Make the request
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=access_token,
-    resource_owner_secret=access_token_secret,
-)
-
-# Making the request
-response = oauth.post(
-    "https://api.twitter.com/2/tweets",
-    json=payload,
-)
-
-if response.status_code != 201:
-    raise Exception(
-        f"Request returned an error: {response.status_code} {response.text}")
-
-print("Response code: {response.status_code}")
-
-# Saving the response as JSON
-json_response = response.json()
-print(json.dumps(json_response, indent=4, sort_keys=True))
+twitter = Twython(consumer_key, consumer_secret, access_token,
+                  access_token_secret)
+try:
+    tweet = twitter.get_home_timeline()[1]
+    print("Tweet text: ", tweet["text"])
+    print("Tweet created at: ", tweet["created_at"])
+    print("Tweeted by: ", tweet["entities"]["user_mentions"][0]["name"])
+    print("Re Tweeted?: ", tweet["retweet_count"])
+except twython.exceptions.TwythonError as err:
+    print(str(err))
