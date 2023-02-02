effb8a25f58ca1ac18533a5fc087d589d17a47e8
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-02 14:21:12 +0530

    fix: fix pylint violations

--- Regex/intro.py
+++ Regex/intro.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
+"""Regex samples"""
 # -*- coding: utf-8 -*-
-sentence = 'This is a sample string'
-# check if 'sentence' contains the given string argument
-print('is' in sentence)
-print('xyz' in sentence)
-# need to load the re module before use
 import re
-# check if 'sentence' contains the pattern described by RE argument
-print(bool(re.search(r'is', sentence)))
-print(bool(re.search(r'xyz', sentence)))
 
-if re.search(r'ring', sentence):
+SENTENCE = 'This is a sample string'
+# check if 'SENTENCE' contains the given string argument
+print('is' in SENTENCE)
+print('xyz' in SENTENCE)
+# need to load the re module before use
+# check if 'SENTENCE' contains the pattern described by RE argument
+print(bool(re.search(r'is', SENTENCE)))
+print(bool(re.search(r'xyz', SENTENCE)))
+
+if re.search(r'ring', SENTENCE):
     print('mission success')
-if not re.search(r'xyz', sentence):
+if not re.search(r'xyz', SENTENCE):
     print('mission failed')
 words = ['cat', 'attempt', 'tattle']
 print([w for w in words if re.search(r'tt', w)])
