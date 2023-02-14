e4ab864766eed4cd56e63cf47095deb5a05d4dcf
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-14 18:52:55 +0530

    feat: add backreferences example

--- Regex/backreferences.py
+++ Regex/backreferences.py
@@ -9,6 +9,7 @@
 ######################################################################
 """
 import re
+
 # remove square brackets that surround digit characters
 print(re.sub(r'\[(\d+)\]', r'\1', '[52] apples and [31] mangoes'))
 # replace __ with _ and delete _ if it is alone
@@ -20,3 +21,32 @@ print(re.sub(r'\d+', r'(\g<0>0)', '52 apples and 31 mangoes'))
 print(re.sub(r'.*', r'Hi,\g<0>. Have a nice day!', 'Linus Fernandes', count=1))
 # swap words that are separated by a comma
 print(re.sub(r'(\w+),(\w+)', r'\2,\1', 'good,bad 42,24'))
+
+# whole words that have at least one consecutive repeated character
+words = ['effort', 'flee', 'facade', 'oddball', 'rat', 'tool']
+print([w for w in words if re.search(r'\b\w*(\w)\1\w*\b', w)])
+# remove any number of consecutive duplicate words separated by space
+# quantifiers can be applied to backreferences too!
+print(re.sub(r'\b(\w+)( \1)+\b', r'\1', 'aa a a a 42 f_1 f_1 f_13.14'))
+
+# without capture group
+print(re.split(r'\d+', 'Sample123string42with777numbers'))
+# to include the matching delimiter strings as well in the output
+print(re.split(r'(\d+)', 'Sample123string42with777numbers'))
+# optional argument maxsplit can be used to specify no. of splits
+# setting to 1 gives behavior like partition string method
+print(re.split(r'(1*2)', '3111111111125111142', maxsplit=1))
+# normal capture group will hinder ability to get whole match
+# non-capturing group to the rescue
+print(re.findall(r'\b\w*(?:st|in)\b', 'cost akin more east run against'))
+# capturing wasn't needed here, only common grouping and quantifier
+print(re.split(r'hand(?:y|ful)?', '123hand42handy777handful500'))
+# with normal grouping, need to keep track of all the groups
+print(re.sub(r'\A(([^,]+,){3})([^,]+)', r'\1(\3)', '1,2,3,4,5,6,7', count=1))
+# using non-capturing groups, only relevant groups have to be tracked
+print(re.sub(r'\A((?:[^,]+,){3})([^,]+)', r'\1(\2)', '1,2,3,4,5,6,7', count=1))
+WORDS = 'effort flee facade oddball rat tool'
+# whole words containing at least one consecutive repeated character
+repeat_char = re.compile(r'\b\w*(\w)\1\w*\b')
+# () in findall will only return text matched by capture groups
+print(repeat_char.findall(WORDS))
