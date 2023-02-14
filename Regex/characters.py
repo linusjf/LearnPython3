86779930491446306e5fabdc575466ebf468797c
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-14 10:33:03 +0530

    feat: add character text cheatsheet

--- Regex/characters.py
+++ Regex/characters.py
@@ -57,3 +57,28 @@ print(re.findall(r'[^0-9]+', 'Sample123string42with777numbers'))
 print(re.sub(r'\A([^:]+:){2}', r'', 'foo:123:bar:baz', count=1))
 # deleting characters at end of string based on a delimiter
 print(re.sub(r'=[^=]+\Z', r'', 'foo=42; baz=123', count=1))
+
+words = ['tryst', 'fun', 'glyph', 'pity', 'why']
+# words not containing vowel characters
+print([w for w in words if re.search(r'\A[^aeiou]+\Z', w)])
+# easier to write and maintain
+print([w for w in words if not re.search(r'[aeiou]', w)])
+# - should be first or last character or escaped using \
+print(re.findall(r'\b[a-z-]{2,}\b', 'ab-cd gh-c 12-423'))
+print(re.findall(r'\b[a-z\-0-9]{2,}\b', 'ab-cd gh-c 12-423'))
+# ^ should be other than first character or escaped using \
+print(re.findall(r'a[+^]b', 'f*(a^b) - 3*(a+b)'))
+print(re.findall(r'a[\^+]b', 'f*(a^b) - 3*(a+b)'))
+# [ can be escaped with \ or placed as last character
+# ] can be escaped with \ or placed as first character
+print(re.search(r'[a-z\[\]0-9]+', 'words[5] = tea')[0])
+# \ should be escaped using \
+print(re.search(r'[a\\b]+', r'5ba\babc2')[0])
+
+print(re.split(r'\d+', 'Sample123string42with777numbers'))
+print(re.findall(r'\d+', 'foo=5, bar=3; x=83, y=120'))
+print(''.join(re.findall(r'\b\w', 'sea eat car rat eel tea')))
+print(re.findall(r'[\w\s]+', 'tea sea-pit sit-lean\tbean'))
+print(re.sub(r'\D+', r'-', 'Sample123string42with777numbers'))
+print(re.sub(r'\W+', r'', 'foo=5, bar=3; x=83, y=120'))
+print(re.findall(r'\S+', ' 1..3 \v\f foo_baz 42\tzzz \r\n1-2-3 '))
