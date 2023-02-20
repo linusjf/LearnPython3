eaecf81defcc5e2cb5f5ed48a51e56c7a77e4752
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 19:53:01 +0530

    fix: fix pylint violations

--- AutomateIt/tornadoserver.py
+++ AutomateIt/tornadoserver.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""TornadoServer."""
 import tornado.ioloop
 import tornado.web
+import tornado.gen
 import httplib2
 
 
+# pylint: disable=abstract-method
 class AsyncHandler(tornado.web.RequestHandler):
+    """AsyncHandler class."""
+
+    def __init__(self):
+        """Construct."""
+        super().__init__(self, None)
+        self.response = None
+        self.content = None
+
     @tornado.gen.coroutine
     def get(self):
+        """Get."""
         http = httplib2.Http()
-        self.response, self.content = http.request("http://ip.jsontest.com/", "GET")
+        self.response, self.content = http.request("http://ip.jsontest.com/", "GET")  # noqa
         self._async_callback(self.response, self.content)
 
     def _async_callback(self, response, content):
+        """Call back asynchronously."""
         print("Content:", content)
         print(
-            "Response:\nStatusCode: %s Location: %s"
-            % (response["status"], response["content-location"])
+            f"Response:\nStatusCode: {response['status']} Location:\
+                    {response['content-location']}"
         )
         self.finish()
         tornado.ioloop.IOLoop.instance().stop()
