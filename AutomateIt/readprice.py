e80d756aec8c3981d57d28e651cc11ef1716ca5a
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 20:38:17 +0530

    fix: fix pylint violations

--- AutomateIt/readprice.py
+++ AutomateIt/readprice.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""ReadPrice."""
 from lxml import html
 import requests
 
-page = requests.get("https://github.com/pricing/")
+page = requests.get("https://github.com/pricing/", timeout=5)
 tree = html.fromstring(page.content)
 print("Page Object:", tree)
 plans = tree.xpath('//h2[@class="mb-2 h5-mktg"]/text()')
-pricing = tree.xpath('//span[@data-plan="free" and @class="js-computed-value"]/text()')[0]
+pricing = tree.xpath('//span[@data-plan=\
+        "free" and @class="js-computed-value"]/text()')[0]
 print("Plan:", plans[0], "\nPricing:", pricing)
-pricing = tree.xpath('//span[@data-plan="business" and @class="js-computed-value"][1]/text()')[0]
+pricing = tree.xpath('//span[@data-plan="business"\
+        and @class="js-computed-value"][1]/text()')[0]
 print("Plan:", plans[1], "\nPricing:", pricing)
 pricing = tree.xpath(
-    '//span[@data-plan="business_plus" and @class="js-computed-value"][1]/text()'
+    '//span[@data-plan="business_plus"\
+            and @class="js-computed-value"][1]/text()'
 )[0]
 print("Plan:", plans[2], "\nPricing:", pricing)
