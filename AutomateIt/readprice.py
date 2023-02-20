6329185bdcd7a4b93e52e51313ec57dc5d2a9799
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 20:51:24 +0530

    fix: fix pylint violations

--- AutomateIt/readprice.py
+++ AutomateIt/readprice.py
@@ -8,11 +8,15 @@ page = requests.get("https://github.com/pricing/", timeout=5)
 tree = html.fromstring(page.content)
 print("Page Object:", tree)
 plans = tree.xpath('//h2[@class="mb-2 h5-mktg"]/text()')
-pricing = tree.xpath('//span[@data-plan=\
-        "free" and @class="js-computed-value"]/text()')[0]
+pricing = tree.xpath(
+    '//span[@data-plan=\
+        "free" and @class="js-computed-value"]/text()'
+)[0]
 print("Plan:", plans[0], "\nPricing:", pricing)
-pricing = tree.xpath('//span[@data-plan="business"\
-        and @class="js-computed-value"][1]/text()')[0]
+pricing = tree.xpath(
+    '//span[@data-plan="business"\
+        and @class="js-computed-value"][1]/text()'
+)[0]
 print("Plan:", plans[1], "\nPricing:", pricing)
 pricing = tree.xpath(
     '//span[@data-plan="business_plus"\
