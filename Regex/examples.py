c2f410698eeb498d36f90ff4eeec9effc089cf2c
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 07:26:09 +0530

    fix: update linter config

--- Regex/examples.py
+++ Regex/examples.py
@@ -10,20 +10,20 @@
 """
 import re
 
-FILENAME = 'programming_quotes.txt'
+FILENAME = "programming_quotes.txt"
 word = re.compile("two")  # add your solution here
-with open(FILENAME, 'r', encoding="utf-8") as ip_file:
+with open(FILENAME, "r", encoding="utf-8") as ip_file:
     for ip_line in ip_file:
         if word.search(ip_line):
-            print(ip_line, end='')
+            print(ip_line, end="")
 
-PURCHASES = '''\
+PURCHASES = """\
 ... apple 24
 ... mango 50
 ... guava 42
 ... onion 31
-... water 10'''
+... water 10"""
 num = re.compile("2")  # add your solution here
-for line in PURCHASES.split('\n'):
+for line in PURCHASES.split("\n"):
     if not num.search(line):
         print(line)
