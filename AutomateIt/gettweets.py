35f77b377dee5604e52b8db7a6814abc9106f0f4
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 15:43:16 +0530

    fix: fix pylint violations

--- AutomateIt/gettweets.py
+++ AutomateIt/gettweets.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 """
+GetTweets.
+
 ######################################################################
 # @author      : Linus Fernandes (linusfernandes at gmail dot com)
 # @file        : gettweets
@@ -16,7 +18,7 @@ bearer_token = config.twitterbearertoken
 
 
 def create_url():
-    """create url"""
+    """Create url."""
     tweet_fields = "tweet.fields=lang,author_id"
     # Tweet fields are adjustable.
     # Options include:
@@ -25,7 +27,8 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    ids = "ids=1278747501642657792,1255542774432063488,1613516569828630529,1570685356110147585"
+    ids = "ids=1278747501642657792,1255542774432063488,1613516569828630529,\
+            1570685356110147585"
     # You can adjust ids to include a single Tweets.
     # Or you can add to up to 100 comma-separated IDs
     url = f"https://api.twitter.com/2/tweets?{ids}&{tweet_fields}"
@@ -33,26 +36,26 @@ def create_url():
 
 
 def bearer_oauth(request):
-    """
-    Method required by bearer token authentication.
-    """
-
+    """Fill request headers with bearer token."""
     request.headers["Authorization"] = f"Bearer {bearer_token}"
     request.headers["User-Agent"] = "v2TweetLookupPython"
     return request
 
 
 def connect_to_endpoint(url):
-    """connect to endpoint"""
+    """Connect to endpoint."""
     response = requests.request("GET", url, auth=bearer_oauth, timeout=10)
     print(response.status_code)
     if response.status_code != 200:
-        raise Exception(f"Request returned an error: {response.status_code} {response.text}")
+        raise Exception(
+            f"Request returned an error: {response.status_code}\
+                {response.text}"
+        )
     return response.json()
 
 
 def main():
-    """main"""
+    """Execute main."""
     url = create_url()
     json_response = connect_to_endpoint(url)
     print(json.dumps(json_response, indent=4, sort_keys=True))
