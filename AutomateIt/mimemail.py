92c76b280153a80db72158649bd21da1d3b4dbe1
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 16:53:24 +0530

    fix: fix pylint violations

--- AutomateIt/mimemail.py
+++ AutomateIt/mimemail.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""MimeMail."""
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import config
 import email.utils
+import config
 
 fromaddr = config.fromaddr
 toaddr = config.toaddr
@@ -13,9 +13,9 @@ msg = MIMEMultipart()
 msg["Subject"] = "Hello from the Reader of Automate It!"
 msg["To"] = email.utils.formataddr(("Recipient", toaddr))
 msg["From"] = email.utils.formataddr(("Author", fromaddr))
-body = "What a wonderful world!"
-msgBody = MIMEText(body, "plain")
-msg.attach(msgBody)
+BODY = "What a wonderful world!"
+msg_body = MIMEText(BODY, "plain")
+msg.attach(msg_body)
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(fromaddr, config.password)
