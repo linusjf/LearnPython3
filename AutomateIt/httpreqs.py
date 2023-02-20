e9a037ed53a0e6257d6ee7f632b9caa1a2050796
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 10:59:51 +0530

    fix: fix pylint violations

--- AutomateIt/httpreqs.py
+++ AutomateIt/httpreqs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+"""htttpreqs"""
 import requests
 
-r = requests.get("http://ip.jsontest.com/")
+r = requests.get("http://ip.jsontest.com/", timeout=5)
 print("Response object:", r)
 print("Response Text:", r.text)
 payload = {"q": "chetan"}
-r = requests.get("https://github.com/search", params=payload)
+r = requests.get("https://github.com/search", params=payload, timeout=5)
 print("Request URL:", r.url)
 payload = {"key1": "value1"}
-r = requests.post("http://httpbin.org/post", data=payload)
+r = requests.post("http://httpbin.org/post", data=payload, timeout=5)
 print("Response text:", r.json())
 try:
-    r = requests.get("http://www.google.com/")
+    r = requests.get("http://www.google.com/", timeout=5)
     print("Response object:", r)
 except requests.exceptions.RequestException as e:
-    print("Error Response:", e.message)
+    print("Error Response:", str(e))
