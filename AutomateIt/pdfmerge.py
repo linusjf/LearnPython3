f0d46877ed7a5aa9a161c9df1ad4dc7dcc45a4b8
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 18:36:24 +0530

    fix: fix pylint violations

--- AutomateIt/pdfmerge.py
+++ AutomateIt/pdfmerge.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from PyPDF2 import PdfReader, PdfMerger
+"""PDFMerge."""
 import os
+from PyPDF2 import PdfReader, PdfMerger
 
 merger = PdfMerger()
 files = [x for x in os.listdir(".") if x.endswith(".pdf")]
 for fname in sorted(files):
-    merger.append(PdfReader(open(os.path.join(".", fname), "rb")))
+    with open(os.path.join(".", fname), "rb", encoding="utf-8") as f:
+        merger.append(PdfReader(f))
 merger.write("output.pdf")
