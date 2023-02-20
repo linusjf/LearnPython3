85918b4a09ae333da21fcfeabeede974dd6f712c
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 08:49:29 +0530

    fix: fix pylint violations

--- AutomateIt/avatar.py
+++ AutomateIt/avatar.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
+"""avatar"""
 import re
 import urllib.request as req
 import os
+from bs4 import BeautifulSoup
 
-## Download paramters
-image_type = "Project"
-movie = "Avatar"
-url = "https://www.google.com/search?q=" + movie + "&source=lnms&tbm=isch"
-header = {"User-Agent": "Mozilla/5.0"}
-soup = BeautifulSoup(req.urlopen(req.Request(url, headers=header)), "lxml")
-images = [a["src"] for a in soup.find_all("img", {"src": re.compile("gstatic.com")})][:5]
-for img in images:
-    print("Image Source:", img)
-    raw_img = req.urlopen(img).read()
-    cntr = len([i for i in os.listdir(".") if image_type in i]) + 1
-    f = open(image_type + "_" + str(cntr) + ".jpg", "wb")
-    f.write(raw_img)
-    f.close()
+# Download paramters
+IMAGE_TYPE = "Project"
+MOVIE = "Avatar"
+URL = "https://www.google.com/search?q=" + MOVIE + "&source=lnms&tbm=isch"
+HEADER = {"User-Agent": "Mozilla/5.0"}
+with req.urlopen(req.Request(URL, headers=HEADER)) as doc:
+    soup = BeautifulSoup(doc, "lxml")
+    imglines = soup.find_all("img", {"src": re.compile("gstatic.com")})[:5]
+    images = [a["src"] for a in imglines]
+    for img in images:
+        print("Image Source:", img)
+        with req.urlopen(img).read() as raw_img:
+            cntr = len([i for i in os.listdir(".") if IMAGE_TYPE in i]) + 1
+            with open(IMAGE_TYPE + "_" + str(cntr) + ".jpg", "wb") as f:
+                f.write(raw_img)
