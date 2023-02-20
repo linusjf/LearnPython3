82781dfe4941ba564de195b4852d4164a425991e
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 09:22:32 +0530

    fix: fix pylint violations

--- AutomateIt/findsoup.py
+++ AutomateIt/findsoup.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
+"""findsoup"""
 # -*- coding: utf-8 -*-
 import bs4
 
-myfile = open("python.html")
-soup = bs4.BeautifulSoup(myfile, "lxml")
-# Making the soup
-print("BeautifulSoup Object:", type(soup))
-# Find Elements By tags
-print(soup.find_all("a"))
-print(soup.find_all("strong"))
-# Find Elements By id
-print(soup.find("div", {"id": "inventor"}))
-print(soup.select("#inventor"))
-# Find Elements by css print
-print(soup.select(".wow"))
-print("Facebook URL:", soup.find_all("a")[0]["href"])
-print("Inventor:", soup.find("div", {"id": "inventor"}).text)
-print("Span content:", soup.select("span")[0].getText())
+with open("python.html", encoding="utf-8") as myfile:
+    soup = bs4.BeautifulSoup(myfile, "lxml")
+    # Making the soup
+    print("BeautifulSoup Object:", type(soup))
+    # Find Elements By tags
+    print(soup.find_all("a"))
+    print(soup.find_all("strong"))
+    # Find Elements By id
+    print(soup.find("div", {"id": "inventor"}))
+    print(soup.select("#inventor"))
+    # Find Elements by css print
+    print(soup.select(".wow"))
+    print("Facebook URL:", soup.find_all("a")[0]["href"])
+    print("Inventor:", soup.find("div", {"id": "inventor"}).text)
+    print("Span content:", soup.select("span")[0].getText())
