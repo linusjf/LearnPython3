5f8edb676e7423c7b9485c03d8ed728ca57db93c
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 18:36:24 +0530

    fix: fix pylint violations

--- AutomateIt/pdfhdftr.py
+++ AutomateIt/pdfhdftr.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""PDFHeaderFooter."""
 from fpdf import FPDF
 
 
 class PDF(FPDF):
+    """PDF Class."""
+
     def footer(self):
+        """Create footer."""
         self.set_y(-15)
         self.set_font("Arial", "I", 8)
-        self.cell(0, 10, "Page %s" % self.page_no(), 0, 0, "C")
+        self.cell(0, 10, f"Page {self.page_no()}", 0, 0, "C")
 
     def header(self):
+        """Create header."""
         self.set_font("Arial", "B", 15)
         self.cell(80)
         self.cell(30, 10, "Automate It!", 1, 0, "C")
@@ -20,5 +25,5 @@ pdf = PDF(format="A5")
 pdf.add_page()
 pdf.set_font("Times", size=12)
 for i in range(1, 50):
-    pdf.cell(0, 10, "This my new line. line number is: %s" % i, ln=1, align="C")
+    pdf.cell(0, 10, f"This my new line. Line number is: {i}", ln=1, align="C")
 pdf.output("header_footer.pdf")
