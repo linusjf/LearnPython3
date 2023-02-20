a177dc490bd573c4757119b08aea29e7e2c21e06
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 19:17:12 +0530

    fix: fix pylint violations

--- AutomateIt/writecsv.py
+++ AutomateIt/writecsv.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""WriteCSV."""
 import csv
 
 names = ["John", "Eve", "Fate", "Jadon"]
-
 grades = ["C", "A+", "A", "B-"]
-f = open("newlist.csv", "wt")
-try:
+with open("newlist.csv", "wt", encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerow(("Sr.", "Names", "Grades"))
     for i in range(4):
         writer.writerow((i + 1, names[i], grades[i]))
-finally:
-    f.close()
