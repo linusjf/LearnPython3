bef385f9ceb340c0059d9b0a8392111f28da3ec5
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-20 20:51:24 +0530

    fix: fix pylint violations

--- AutomateIt/readdict.py
+++ AutomateIt/readdict.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""ReadDict."""
 import csv
 
-f = open("mylist.csv", "rt")
-print("File Contents:")
-try:
+with open("mylist.csv", "rt", encoding="utf-8") as f:
+    print("File Contents:")
     reader = csv.DictReader(f)
     for row in reader:
         print(row["first_name"], row["last_name"], row["email"])
-finally:
-    f.close()
 
-f = open("mylist.csv", "rt")
-reader = csv.DictReader(f)
-print("Columns in CSV file:", reader.fieldnames)
-print("Dialect used in CSV file:", reader.dialect)
-print("Current line number in CSV file:", reader.line_num)
-print("Moving the reader to next line with reader.__next__()")
-reader.__next__()
-print("Reading line number:", reader.line_num)
-f.close()
+with open("mylist.csv", "rt", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    print("Columns in CSV file:", reader.fieldnames)
+    print("Dialect used in CSV file:", reader.dialect)
+    print("Current line number in CSV file:", reader.line_num)
+    print("Moving the reader to next line with reader.__next__()")
+    next(reader)
+    print("Reading line number:", reader.line_num)
