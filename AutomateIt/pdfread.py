06b723f6990ae28583542c71246ed62f052a3d34
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-21 10:48:20 +0530

    fix: fix pylint violations

--- AutomateIt/pdfread.py
+++ AutomateIt/pdfread.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import PyPDF2
+"""PDFRead."""
 from PyPDF2 import PdfReader
 
-pdf = open("diveintopython.pdf", "rb")
-readerObj = PdfReader(pdf, strict=False)
-print("PDF Reader Object is:", readerObj)
-print("Details of diveintopython book")
-print("Number of pages:", len(readerObj.pages))
-print("Title:", readerObj.metadata.title)
-print("Author:", readerObj.metadata.author)
+with open("diveintopython.pdf", "rb", encoding="utf-8") as pdf:
+    reader = PdfReader(pdf, strict=False)
+    print("PDF Reader Object is:", reader)
+    print("Details of diveintopython book")
+    print("Number of pages:", len(reader.pages))
+    print("Title:", reader.metadata.title)
+    print("Author:", reader.metadata.author)
 
-print("Reading Page 1")
-page = readerObj.pages[0]
-print(page.extract_text())
+    print("Reading Page 1")
+    page = reader.pages[0]
+    print(page.extract_text())  # pylint: disable=no-member
 
-print("Book Outline")
-for heading in readerObj.outline:
-    if type(heading) is not list:
+    print("Book Outline")
+for heading in reader.outline:
+    if not isinstance(heading, list):
         print(dict(heading).get("/Title"))
