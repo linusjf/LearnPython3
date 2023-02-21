2714c8a5437c22137c1a64888b7edae000e4bd91
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-21 16:48:04 +0530

    fix: fix lint violations

--- AutomateIt/imapmail.py
+++ AutomateIt/imapmail.py
@@ -5,15 +5,17 @@ import imaplib
 import config
 
 M = imaplib.IMAP4_SSL("imap.gmail.com", 993)
-M.login(config.fromaddr, config.password)
-print(M.list())
-M.select("INBOX")
-print("Inbox:", M)
-typ, data = M.search(None, "SUBJECT", '"Email with an attachment"')
-print(typ)
-print(data)
-if data[0] != b"":
-    typs, msg = M.fetch(data[0].split()[-1], "(RFC822)")
-    print("Message is ", msg[0][1])
-M.close()
-M.logout()
+if config.fromaddr and config.password:
+    M.login(config.fromaddr, config.password)
+    print(M.list())
+    M.select("INBOX")
+    print("Inbox:", M)
+    typ, data = M.search(None, "SUBJECT", '"Email with an attachment"')
+    print(typ)
+    print(data)
+    if data[0] != b"":
+        typs, msg = M.fetch(data[0].split()[-1], "(RFC822)")
+        if msg and msg[0]:
+            print("Message is ", msg[0][1])
+    M.close()
+    M.logout()
