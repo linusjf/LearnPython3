b44129a1bba129a751983c40329b7d9d700c93aa
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-21 21:41:18 +0530

    fix: fix lint violations

--- AutomateIt/sendmail.py
+++ AutomateIt/sendmail.py
@@ -7,8 +7,9 @@ import config
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
-server.login(config.fromaddr, config.password)
-MSG = "Some nice msg"
-if config.fromaddr and config.toaddr:
-    server.sendmail(config.fromaddr, config.toaddr, MSG)
+if config.fromaddr and config.password:
+    server.login(config.fromaddr, config.password)
+    MSG = "Some nice msg"
+    if config.fromaddr and config.toaddr:
+        server.sendmail(config.fromaddr, config.toaddr, MSG)
 server.quit()
