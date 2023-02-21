ceddad4930ba70aadf35e98895745b8390661799
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-21 07:11:06 +0530

    feat: add prospector to pre-commit

--- AutomateIt/pdfpayslip.py
+++ AutomateIt/pdfpayslip.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""PDFPaySlip."""
 from datetime import datetime
+from fpdf import FPDF, HTMLMixin
+from fpdf.enums import XPos, YPos, Align
 
 employee_data = [
     {"id": 123, "name": "John Sally", "payment": 10000, "tax": 3000, "total": 7000},
     {"id": 245, "name": "Robert Langford", "payment": 12000, "tax": 4000, "total": 8000},
 ]
 
-from fpdf import FPDF, HTMLMixin
-from fpdf.enums import XPos, YPos, Align
-
 
 class PaySlip(FPDF, HTMLMixin):
+    """PaySlip."""
+
     def footer(self):
+        """Draw footer."""
         self.set_y(-15)
         self.set_font("Courier", "I", 8)
-        self.cell(
-            0, 10, "Page %s" % self.page_no(), 0, new_x=XPos.RIGHT, new_y=YPos.TOP, align="C"
-        )
+        self.cell(0, 10, f"Page {self.page_no()}", 0, new_x=XPos.RIGHT, new_y=YPos.TOP, align="C")
 
     def header(self):
+        """Draw header."""
         self.set_font("Courier", "B", 15)
         self.cell(80)
         self.cell(30, 10, "Google", 0, new_x=XPos.RIGHT, new_y=YPos.TOP, align=Align.C)
@@ -28,23 +29,24 @@ class PaySlip(FPDF, HTMLMixin):
 
 
 def generate_payslip(data):
+    """Generate payslip."""
     month = datetime.now().strftime("%B")
     year = datetime.now().strftime("%Y")
     pdf = PaySlip(format="letter")
     pdf.add_page()
     pdf.set_font("Times", size=12)
-    pdf.cell(200, 10, txt="Pay Slip for %s, %s" % (month, year), align=Align.C)
+    pdf.cell(200, 10, txt=f"Pay Slip for {month}, {year}", align=Align.C)
     pdf.cell(50)
     pdf.cell(
         100,
         10,
-        txt="Employeed Id: %s" % data["id"],
+        txt=f"Employeed Id: {data['id']}",
         new_x=XPos.LMARGIN,
         new_y=YPos.NEXT,
         align="L",
     )
     pdf.cell(50)
-    pdf.cell(100, 10, txt="Employeed Name: %s" % data["name"], align="L")
+    pdf.cell(100, 10, txt=f"Employee Name: {data['name']}", align="L")
     html = (
         """
 <table border="0" align="center" width="50%">
@@ -66,7 +68,7 @@ Amount in USD</th></tr></thead>
 """
     )
     pdf.write_html(html)
-    pdf.output("payslip_%s.pdf" % data["id"])
+    pdf.output("payslip_{data['id']}.pdf")
 
 
 for emp in employee_data:
