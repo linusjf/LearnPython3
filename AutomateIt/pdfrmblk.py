96fe8d4cefa36d08c505ca795b205542e8769ba4
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-21 10:48:20 +0530

    fix: fix pylint violations

--- AutomateIt/pdfrmblk.py
+++ AutomateIt/pdfrmblk.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""PDFRmBlk."""
 
 from PyPDF2 import PdfReader, PdfWriter
 
 infile = PdfReader("myPdf.pdf", "rb")
 output = PdfWriter()
-for i in range(len(infile.pages)):
-    p = infile.pages[i]
+for _, p in enumerate(infile.pages):
     if p.get_contents():
         output.add_page(p)
 with open("myPdf_wo_blank.pdf", "wb") as f:
