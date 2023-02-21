7c51e517ab81027dd70064859e99c3d3a529654b
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-21 10:35:12 +0530

    fix: fix pylint violations

--- AutomateIt/pdfrotate.py
+++ AutomateIt/pdfrotate.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""PDFRotate."""
 from PyPDF2 import PdfReader, PdfWriter
 
-fp = open("diveintopython.pdf", "rb")
-readerObj = PdfReader(fp)
-page = readerObj.pages[0]
-page.rotate(90)
-writer = PdfWriter()
-writer.add_page(page)
-fw = open("rotated.pdf", "wb")
-writer.write(fw)
-fw.close()
-fp.close()
+with open("diveintopython.pdf", "rb", encoding="utf-8") as fp:
+    reader = PdfReader(fp)
+    page = reader.pages[0]
+    page.rotate(90)  # pylint: disable=no-member
+    writer = PdfWriter()
+    writer.add_page(page)
+    with open("rotated.pdf", "wb", encoding="utf-8") as fw:
+        writer.write(fw)
