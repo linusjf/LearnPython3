ebd2fefcf9570ddb9865ef1d4b606a7a5698e30d
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-25 12:29:40 +0530

    fix: fix spaces in line overlapped strings

--- AutomateIt/readtoptweet.py
+++ AutomateIt/readtoptweet.py
@@ -22,7 +22,7 @@ consumer_secret = config.twitterapikeysecret
 
 # Get request token
 REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token?\
-            oauth_callback=oob&x_auth_access_type=write"
+oauth_callback=oob&x_auth_access_type=write"  # nosec
 oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
 
 try:
