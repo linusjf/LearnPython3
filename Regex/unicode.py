0bc68110b39e7614a2e5072590c44358436a6caa
Linus Fernandes <linus.fernandes@gmail.com>
2023-02-27 19:57:02 +0530

    feat: add unicode cheatsheet

--- Regex/unicode.py
+++ Regex/unicode.py
@@ -40,3 +40,53 @@ print([c.encode("unicode_escape") for c in "İıſK"])
 # character range example using \u
 # all english lowercase letters
 print(re.findall(r"[\u0061-\u007a]+", "fox:αλεπού,eagle:αετός"))
+
+print()
+print("Exercises")
+print()
+
+# Exercises
+# a) Output True or False depending on input string
+# made up of ASCII characters or not.
+# Consider the input to be non-empty strings and any
+# character that isn’t part of 7-bit ASCII set
+# should give False
+STR1 = "123—456"
+STR2 = "good fοοd"
+STR3 = "happy learning!"
+STR4 = "İıſK"
+STR5 = "àpple"
+print(not bool(re.search(r"[^\x00-\x7f]", STR1)))
+print(not bool(re.search(r"[^\x00-\x7f]", STR2)))
+print(not bool(re.search(r"[^\x00-\x7f]", STR3)))
+print(not bool(re.search(r"[^\x00-\x7f]", STR4)))
+print(not bool(re.search(r"[^\x00-\x7f]", STR5)))
+print(STR1.isascii())
+print(STR2.isascii())
+print(STR3.isascii())
+print(STR4.isascii())
+print(STR5.isascii())
+print([c.encode("unicode_escape") for c in STR1])
+print([c.encode("unicode_escape") for c in STR2])
+print([c.encode("unicode_escape") for c in STR3])
+print([c.encode("unicode_escape") for c in STR4])
+print([c.encode("unicode_escape") for c in STR5])
+
+# b) Does the . quantifier match non-ASCII characters even with
+# the re.ASCII flag enabled?
+m = re.search(r".+", "fox:αλεπού")
+if m:
+    print(m[0])
+
+m = re.search(r"(?a).+", "fox:αλεπού")
+if m:
+    print(m[0])
+
+subscripts = re.compile(r"[₁₂₃₄₅₆₇₈₉₀]")
+STR = "a₅b₁₀"
+print(subscripts.findall(STR))
+subscripts = re.compile(r"[₁₂₃₄₅₆₇₈₉₀]+")
+print(subscripts.findall(STR))
+superscripts = re.compile(r"\d+[²³¹⁰ⁱ⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾ⁿ]+")
+STR = "N = 2¹⁰"
+print(superscripts.findall(STR))
