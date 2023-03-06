d8c9e4c75cba3236242d5f853d936d7e8acad828
Linus Fernandes <linus.fernandes@gmail.com>
2023-03-06 19:56:49 +0530

    feat: add control verbs examples

--- Regex/controlverbs.py
+++ Regex/controlverbs.py
@@ -20,31 +20,57 @@ if m:
     print(m[0])
 S1 = """
 Quincy Jones: music producer
-Rashida Jones: actress
 Peter Sellers: comedian
-Indiana Jones: movie character
+Rashida Jones: actress
 Jerry Seinfeld: comedian
+Indiana Jones: movie character
 Bill Burr: comedian
 Eddie Murphy: actor-comedian
 """
+S2 = """Quincy Jones: music producer
+Peter Sellers: comedian
+Rashida Jones: actress
+Jerry Seinfeld: comedian
+Indiana Jones: movie character
+Bill Burr: comedian
+Eddie Murphy: actor-comedian"""
+S3 = """Peter Sellers: comedian
+Quincy Jones: music producer
+Rashida Jones: actress
+Jerry Seinfeld: comedian
+Indiana Jones: movie character
+Bill Burr: comedian
+Eddie Murphy: actor-comedian"""
 PAT = r"(?ms)(^(?>\w{6,}) Jones:[^\r\n]+)+"
 regexx = regex.compile(PAT)
 m = regexx.search(S1)
 if m:
     print(m[0])
 print(regexx.findall(S1))
+m = regexx.search(S2)
+if m:
+    print(m[0])
+print(regexx.findall(S2))
 PAT = r"(?ms)(^\w{6,}+ Jones:[^\r\n]+)+"
 regexx = regex.compile(PAT)
 m = regexx.search(S1)
 if m:
     print(m[0])
 print(regexx.findall(S1))
+m = regexx.search(S2)
+if m:
+    print(m[0])
+print(regexx.findall(S2))
 PAT = r"(?ms)(^\w{6,}(*PRUNE) Jones:[^\r\n]+)+"
 regexx = regex.compile(PAT)
 m = regexx.search(S1)
 if m:
     print(m[0])
 print(regexx.findall(S1))
+m = regexx.search(S2)
+if m:
+    print(m[0])
+print(regexx.findall(S2))
 
 ATOMIC = r"(?>\w{2,4} )Murray|Bill Burr|Peter Sellers"
 print(regex.search(ATOMIC, "Bill Burr -- Peter Sellers"))
@@ -56,3 +82,24 @@ m = regexx.search(S1)
 if m:
     print(m[0])
 print(regexx.findall(S1))
+m = regexx.search(S2)
+if m:
+    print(m[0])
+print(regexx.findall(S2))
+PAT = r"(?ms)(?:(?:(?=^\w{6,} Jones:)[^\r\n]+\r?\n?)(*SKIP)(*FAIL)|([^\r\n]+)\r?\n?)"
+regexx = regex.compile(PAT)
+m = regexx.search(S1)
+if m:
+    print(m.groups(0))
+print(regexx.findall(S1))
+print(regexx.sub("", S1))
+m = regexx.search(S2)
+if m:
+    print(m.groups(0))
+print(regexx.findall(S2))
+print(regexx.sub("", S2))
+m = regexx.search(S3)
+if m:
+    print(m.groups(0))
+print(regexx.findall(S3))
+print(regexx.sub("", S3))
