b7db6ee297d36b6ca2069dc414d6266fb1877b88
Linus Fernandes <linus.fernandes@gmail.com>
2023-03-06 21:57:43 +0530

    feat: start dynamic programming

---
+++ DynamicProgramming/fibonacci.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Fibonacci.
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : fibonacci
+# @created     : Monday Mar 06, 2023 21:19:48 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+"""
+from timeit import timeit
+
+
+def fib(_n, _dp=None):
+    """Fibonacci."""
+    if _n in (1, 2):
+        return 1
+    if _dp is None:
+        _dp = [-1] * _n
+    if _dp[_n - 1] != -1:
+        return _dp[_n - 1]
+    _dp[_n - 1] = fib(_n - 1, _dp) + fib(_n - 2, _dp)
+    return _dp[_n - 1]
+
+
+print(timeit("fib(100)", number=100000, globals=globals()))
