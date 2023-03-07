6aac2f0a10bbe622b2628cbaeabbba76a56326a1
Linus Fernandes <linus.fernandes@gmail.com>
2023-03-07 20:43:42 +0530

    feat: start dynamic programming

--- DynamicProgramming/fibonacci.py
+++ DynamicProgramming/fibonacci.py
@@ -13,6 +13,13 @@ Fibonacci.
 from timeit import timeit
 
 
+def naivefib(_n):
+    """Naive Fibonacci."""
+    if _n <= 2:
+        return 1
+    return naivefib(_n - 1) + naivefib(_n - 2)
+
+
 def fib(_n, _dp=None):
     """Fibonacci."""
     if _n in (1, 2):
@@ -25,4 +32,21 @@ def fib(_n, _dp=None):
     return _dp[_n - 1]
 
 
-print(timeit("fib(100)", number=100000, globals=globals()))
+def spacefib(_n):  # noqa
+    """Space Fibonacci."""
+    _a = 0
+    _b = 1
+    if _n == 0:
+        return _a
+    if _n in (1, 2):
+        return _b
+    for _i in range(2, _n + 1):
+        _c = _a + _b
+        _a = _b
+        _b = _c
+    return _b
+
+
+print(timeit("naivefib(15)", number=10000, globals=globals()))
+print(timeit("fib(15)", number=10000, globals=globals()))
+print(timeit("spacefib(15)", number=10000, globals=globals()))
