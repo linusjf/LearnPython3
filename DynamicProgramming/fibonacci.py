4f8024feed75f0dafab8062e1363d6e02024a5ce
Linus Fernandes <linus.fernandes@gmail.com>
2023-03-08 05:30:39 +0530

    feat: start dynamic programming

--- DynamicProgramming/fibonacci.py
+++ DynamicProgramming/fibonacci.py
@@ -47,6 +47,41 @@ def spacefib(_n):  # noqa
     return _b
 
 
+def powerfib(num):  # noqa
+    """Power Fib."""
+    fibmat = [[1, 1], [1, 0]]
+    if num == 0:
+        return 0
+    power(fibmat, num - 1)
+    return fibmat[0][0]
+
+
+def multiply(fibmat, mat):
+    """Multiply."""
+    xval = fibmat[0][0] * mat[0][0] + fibmat[0][1] * mat[1][0]
+
+    yval = fibmat[0][0] * fibmat[0][1] + fibmat[0][1] * fibmat[1][1]
+
+    zval = fibmat[1][0] * fibmat[0][0] + fibmat[1][1] * fibmat[1][0]
+
+    wval = fibmat[1][0] * mat[0][1] + fibmat[1][1] * mat[1][1]
+
+    fibmat[0][0] = xval
+    fibmat[0][1] = yval
+    fibmat[1][0] = zval
+    fibmat[1][1] = wval
+
+
+def power(fibmat, num):
+    """Power."""
+    mat = [[1, 1], [1, 0]]
+    # n - 1 times multiply the
+    # matrix to {{1,0},{0,1}}
+    for _ in range(2, num + 1):
+        multiply(fibmat, mat)
+
+
 print(timeit("naivefib(15)", number=10000, globals=globals()))
 print(timeit("fib(15)", number=10000, globals=globals()))
 print(timeit("spacefib(15)", number=10000, globals=globals()))
+print(timeit("powerfib(15)", number=10000, globals=globals()))
