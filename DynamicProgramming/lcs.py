c970b405137eab860e665e1bbdc38e62560a008f
Linus Fernandes <linus.fernandes@gmail.com>
2023-03-09 11:10:38 +0530

    fix: add print lcs

--- DynamicProgramming/lcs.py
+++ DynamicProgramming/lcs.py
@@ -41,9 +41,46 @@ def lcs(str1, str2, dparr=None):
     return dparr[index1][index2]
 
 
+def lcsopt(str1, str2, dparr=None):
+    """Compute longest common subsequence optimally."""
+    # find the length of the strings
+    index1 = len(str1)
+    index2 = len(str2)
+    # declaring the array for storing the dp values
+    if dparr is None:
+        dparr = [[0] * (index2 + 1) for _ in range(2)]
+    for i in range(index1 + 1):
+        for j in range(index2 + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dparr[i % 2][j] = dparr[(i - 1) % 2][j - 1] + 1
+            else:
+                dparr[i % 2][j] = max(dparr[(i - 1) % 2][j], dparr[i % 2][j - 1])
+    return dparr[index1 % 2][index2]
+
+
+def lcsopt2(str1, str2, dparr=None):
+    """Compute longest common subsequence optimally again."""
+    # find the length of the strings
+    index1 = len(str1)
+    index2 = len(str2)
+    # declaring the array for storing the dp values
+    if dparr is None:
+        dparr = [0] * (index2 + 1)
+    for i in range(1, index1 + 1):
+        prev_row = 0
+        prev_row_prev_col = 0
+        for j in range(1, index2 + 1):
+            prev_row_prev_col = prev_row
+            prev_row = dparr[j]
+            if str1[i - 1] == str2[j - 1]:
+                dparr[j] = prev_row_prev_col + 1
+            else:
+                dparr[j] = max(dparr[j - 1], prev_row)
+    return dparr[index2]
+
+
 def print_lcs(table, string1, string2):
     """Print lcs."""
-    print(table)
     stack = []
     i = len(string1)
     j = len(string2)
@@ -64,6 +101,10 @@ def print_lcs(table, string1, string2):
 X = "AGGTAB"
 Y = "GXTXAYB"
 print(X, Y)
+print(lcsopt2(X, Y))
+print(timeit("lcsopt2(X,Y)", number=10000, globals=globals()))
+print(lcsopt(X, Y))
+print(timeit("lcsopt(X,Y)", number=10000, globals=globals()))
 cache = [[-1] * (len(Y) + 1) for _ in range(len(X) + 1)]
 print(lcs(X, Y, cache))
 print_lcs(cache, X, Y)
