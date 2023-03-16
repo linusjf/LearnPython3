c26ccedee4b10ef1c8aa12b1b454865ae4d333c3
Linus Fernandes <linus.fernandes@gmail.com>
2023-03-16 17:01:00 +0530

    feat: add check obstacles

--- DynamicProgramming/obstacles.py
+++ DynamicProgramming/obstacles.py
@@ -10,10 +10,14 @@ Obstacles.
 # -*- coding: utf-8 -*-'
 ######################################################################
 """
+from timeit import timeit
+from functools import lru_cache
 
 
-def unique_paths(i, j, posx, posy, array):
+@lru_cache
+def unique_paths(i, j, array):
     """Solve for unique paths."""
+    posx, posy = len(array), len(array[0])
     # boundary condition or constraints
     if i == posx or j == posy:
         return 0
@@ -22,24 +26,48 @@ def unique_paths(i, j, posx, posy, array):
 
     if i == posx - 1 and j == posy - 1:
         return 1
-    return sum(
-        [unique_paths(i + 1, j, posx, posy, array), unique_paths(i, j + 1, posx, posy, array)]
-    )
+    return sum([unique_paths(i + 1, j, array), unique_paths(i, j + 1, array)])
+
+
+def unique_paths_td(i, j, array, paths=None):
+    """Solve for unique paths top-down."""
+    posx, posy = len(array), len(array[0])
+    # boundary condition or constraints
+    if i == posx or j == posy:
+        return 0
+    if array[i][j] == 1:
+        return 0
+
+    if i == posx - 1 and j == posy - 1:
+        return 1
+    if paths is None:
+        paths = [[-1] * posy for _ in range(posx)]
+    if paths[i][j] != -1:
+        return paths[i][j]
+    paths[i][j] = unique_paths_td(i + 1, j, array, paths) + unique_paths_td(i, j + 1, array, paths)
+    return paths[i][j]
 
 
 def solve_for_obstacles(array):
     """Solve for obstacles."""
-    xpos, ypos = len(array), len(array[0])
-    return unique_paths(0, 0, xpos, ypos, array)
+    return unique_paths(0, 0, array)
+
+
+def solve_for_obstacles_td(array):
+    """Solve for obstacles."""
+    return unique_paths_td(0, 0, array)
 
 
 # Driver code
 A = [
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
+    tuple([0, 0, 0, 0, 0]),
+    tuple([0, 1, 0, 0, 0]),
+    tuple([0, 0, 1, 0, 0]),
+    tuple([0, 0, 0, 1, 0]),
+    tuple([0, 0, 0, 0, 0]),
+    tuple([0, 0, 0, 0, 0]),
 ]
-print(solve_for_obstacles(A))
+print(solve_for_obstacles(tuple(A)))
+print(timeit("solve_for_obstacles(tuple(A))", number=10000, globals=globals()))
+print(solve_for_obstacles_td(tuple(A)))
+print(timeit("solve_for_obstacles_td(tuple(A))", number=10000, globals=globals()))
