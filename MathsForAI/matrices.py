9518e87b3e0f4174acfb4c4431f504443ddb032c
Linus Fernandes <linus.fernandes@gmail.com>
2023-05-14 21:48:43 +0530

    feat: start MathForAI

---
+++ MathsForAI/matrices.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+Matrices.
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : matrices
+# @created     : Sunday May 14, 2023 20:28:11 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+"""
+import sys
+import numpy as np
+
+print(f"Python version {sys.version}")
+print(f"Numpy version: {np.__version__}")
+# define a scalar
+X = 6
+print(X)
+# define a vector
+XV = np.array((1, 2, 3))
+print(XV)
