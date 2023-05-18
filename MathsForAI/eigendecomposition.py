610469cd468e32aa7768e4ec010ecd1428c658d0
Linus Fernandes <linus.fernandes@gmail.com>
2023-05-18 09:59:52 +0530

    feat: add eigen decomposition example

---
+++ MathsForAI/eigendecomposition.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Eigendecomposition.
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : eigendecomposition
+# @created     : Thursday May 18, 2023 08:42:28 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+"""
+import sys
+import numpy as np
+from numpy import linalg
+
+print(f"Python version {sys.version}")
+print(f"Numpy version: {np.__version__}")
+
+
+# define an array
+A = np.arange(9) - 3
+print(A)
+B = A.reshape((3, 3))
+print(B)
+# Euclidean L2 norm -- default
+print(linalg.norm(A))
+print(linalg.norm(B))
+# Frogenius norm is the L2 norm for a matrix
+print(linalg.norm(B, "fro"))
+# max norm P = infinity
+print(linalg.norm(A, np.inf))
+print(linalg.norm(B, np.inf))
+# L1 norm
+print(linalg.norm(A, 1))
+print(linalg.norm(B, 1))
+# vector normalization - normalization to produce a unit vector
+norm = linalg.norm(A)
+A_unit = A / norm
+print(A_unit)
+# the magnitude of unit vector is 1
+print(linalg.norm(A_unit))
