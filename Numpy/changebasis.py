fd385703a8a346e0009abfc2a7408c50ae4b8460
Linus Fernandes <linus.fernandes@gmail.com>
2023-10-10 14:55:17 +0530

    feat: add change of basis example

--- Numpy/changebasis.py
+++ Numpy/changebasis.py
@@ -31,3 +31,29 @@ print(np.dot(M, B2))
 v2calc = np.dot(M, v1)
 print(v2)
 print(v2calc)
+print(np.dot(B1, v1))
+print(np.dot(B2, v2))
+
+# Case 2: Moving from non-standard basis to standard
+
+# alternate basis
+B1 = np.array([[1, 1], [2, 3]])
+v1 = np.array([[3], [1]])
+# vector v2 in standard basis coordinates
+v2 = np.array([[4], [9]])
+# standard basis
+B2 = np.array([[1, 0], [0, 1]])
+
+# create the transformation matrix
+B2inv = np.linalg.inv(B2)
+print(B2inv)
+M = np.dot(B2inv, B1)
+print(M)
+print(np.dot(M, B2))
+
+# perform change of basis
+v2calc = np.dot(M, v1)
+print(v2)
+print(v2calc)
+print(np.dot(B1, v1))
+print(np.dot(B2, v2))
