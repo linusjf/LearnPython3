e454d31ab60d99264881527805fdf9a05d99d174
Linus Fernandes <linus.fernandes@gmail.com>
2023-10-18 15:18:19 +0530

    feat: start matrices

---
+++ LinearAlgebra/matrices.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Matrices.
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : matrices
+# @created     : Wednesday Oct 18, 2023 15:15:59 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+"""
+import numpy as np
+import pandas as pd
+import seaborn as sns
+A = np.array([[0, 2],  # 1st row
+              [1, 4]])  # 2nd row
+print(f'a 2x2 Matrix:\n{A}')
