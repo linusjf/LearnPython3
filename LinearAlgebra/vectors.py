db3d5eb6c8cc59abdaeded9bc34048fda41544ca
Linus Fernandes <linus.fernandes@gmail.com>
2023-10-18 11:56:49 +0530

    feat: add linear algebra project

--- LinearAlgebra/vectors.py
+++ LinearAlgebra/vectors.py
@@ -12,8 +12,7 @@ Vectors.
 """
 import numpy as np
 import pandas as pd
-import altair as alt
-alt.themes.enable('dark')
+import seaborn as sns
 x = np.array([[1],
               [2],
               [3]])
@@ -50,3 +49,31 @@ print(x.T @ y)
 # To compute the L2 distance between a pair of vectors:
 distance = np.linalg.norm(x-y, 2)
 print(f'L_2 distance : {distance}')
+x, y = np.array([[1], [2]]), np.array([[5], [7]])
+# here we translate the cos(theta) definition
+cos_theta = (x.T @ y) / (np.linalg.norm(x, 2) * np.linalg.norm(y, 2))
+print(f'cos of the angle = {np.round(cos_theta, 3)}')
+# to know the exact value of θ
+# we need to take the trigonometric inverse of the cosine function as:
+cos_inverse = np.arccos(cos_theta)
+print(f'angle in radians = {np.round(cos_inverse, 3)}')
+# from radiants to degrees we can use the following formula:
+degrees = cos_inverse * ((180)/np.pi)
+print(f'angle in degrees = {np.round(degrees, 3)}')
+# Here is an example of orthogonal vectors
+x = np.array([[2], [0]])
+y = np.array([[0], [2]])
+cos_theta = (x.T @ y) / (np.linalg.norm(x, 2) * np.linalg.norm(y, 2))
+print(f'cos of the angle = {np.round(cos_theta, 3)}')
+# We see that this vectors are orthogonal as cosθ=0
+# . This is equal to ≈1.57
+cos_inverse = np.arccos(cos_theta)
+degrees = cos_inverse * ((180)/np.pi)
+print(f'angle in radians = {np.round(cos_inverse, 3)}')
+print(f'angle in degrees ={np.round(degrees, 3)} ')
+df = pd.DataFrame({"x1": [0, 2], "y1": [8, 3], "x2": [0.5, 2], "y2": [0, 3]})
+lp = sns.lineplot(data=df, x="x1", y="y1")
+lp = sns.lineplot(data=df, x="x2", y="y2")
+lp.set(xlabel="x1, x2", ylabel="y1, y2", title="Vectors")
+fig = lp.get_figure()
+fig.savefig("vectors.pdf")
