3408eb7c6ea30b7a719c924dc46985a7335b8e9f
Linus Fernandes <linus.fernandes@gmail.com>
2023-11-12 13:06:26 +0530

    feat: add disc example

--- Numpy/disc.py
+++ Numpy/disc.py
@@ -18,10 +18,14 @@ pp = PdfPages("disc.pdf")
 print("Setup Complete")
 
 x = np.linspace(-1, 1, 500)
+print(x)
 X, Y = np.meshgrid(x, x)
+print(X)
+print(Y)
 
 RADIUS = 0.5
 disc = (X ** 2) + (Y ** 2) < RADIUS ** 2
+print(disc)
 plt.imshow(disc, cmap="gray")
 
 pp.savefig()
