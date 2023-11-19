4671f2d3fe1f8b6a2958bade993aca6e9bcab49c
Linus Fernandes <linus.fernandes@gmail.com>
2023-11-20 03:36:53 +0530

    feat: add unflushed example

---
+++ Processes/unflushedprint.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+Unflushedprint.
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : unflushedprint
+# @created     : Monday Nov 20, 2023 03:32:50 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+"""
+# SuperFastPython.com
+# example of not flushing output when call print() from tasks in new processes
+from time import sleep
+from concurrent.futures import ProcessPoolExecutor
+
+
+def task(value):
+    """custom task that will sleep for a moment"""
+    sleep(value)
+    print(f'Done: {value}')
+
+
+def main():
+    """entry point"""
+    # start the process pool
+    with ProcessPoolExecutor() as executor:
+        executor.map(task, range(5))
+    print('All done!')
+
+
+if __name__ == '__main__':
+    main()
