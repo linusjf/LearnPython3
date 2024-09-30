8bf74cba990865e3a214d623f82a5b43aa70aa37
Linus Fernandes <linus.fernandes@gmail.com>
2024-09-30 21:04:30 +0530

    feat: add scipy example

---
+++ Scipy/testcdf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Testcdf.
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : testcdf
+# @created     : Monday Sep 30, 2024 18:44:28 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+"""
+from scipy import stats
+
+val = stats.f.cdf(0.5, 3, 397)
+print(val)
+val = stats.f.sf(0.5, 3, 397)
+print(val)
