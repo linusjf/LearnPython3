ea162d0c30f0a5358a21df836e698fc781ca1ab0
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-05 08:14:45 +0530

    feat: add y on x and x on y regression example

--- SKLearn/xonyandyonx.py
+++ SKLearn/xonyandyonx.py
@@ -16,17 +16,24 @@ from sklearn.linear_model import LinearRegression
 # Generate data
 np.random.seed(1)
 x = np.random.rand(100)
-noise = np.random.randn(100) / 1
-print(np.std(noise))
-y = 2 * x + noise
+noise = np.random.randn(100)
+denominator = 1.0
 
-# Fit regressions
-x_on_y = LinearRegression(fit_intercept=False).fit(y[:, np.newaxis], x)
-y_on_x = LinearRegression(fit_intercept=False).fit(x[:, np.newaxis], y)
+while (True):
+    noise = noise / denominator
+    print(np.std(noise))
+    y = 2 * x + noise
 
-# Coefficient values
-beta1 = y_on_x.coef_[0]
-alpha1 = x_on_y.coef_[0]
-print(beta1,alpha1)
-# Verify inverse relationship
-print(np.isclose(beta1 * alpha1, 1))  # Should print: True
+    # Fit regressions
+    x_on_y = LinearRegression(fit_intercept=False).fit(y[:, np.newaxis], x)
+    y_on_x = LinearRegression(fit_intercept=False).fit(x[:, np.newaxis], y)
+
+    # Coefficient values
+    beta1 = y_on_x.coef_[0]
+    alpha1 = x_on_y.coef_[0]
+    print(beta1, alpha1)
+    # Verify inverse relationship
+    print(np.isclose(beta1 * alpha1, 1))  # Should print: True
+    if (np.isclose(beta1 * alpha1, 1)):
+        break
+    denominator *= 10
