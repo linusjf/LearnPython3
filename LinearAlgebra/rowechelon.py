fef1ed2aa21f4bf9312c645b7e147c397abd0b07
Linus Fernandes <linus.fernandes@gmail.com>
2025-04-05 14:18:30 +0530

    feat: add row echelon example

---
+++ LinearAlgebra/rowechelon.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Rowechelon.
+
+######################################################################
+# @author      : linusjf (linusjf@JuliusCaesar)
+# @file        : rowechelon
+# @created     : Saturday Apr 05, 2025 13:53:26 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+"""
+import numpy as np
+
+# Function to get the position of pivot in a row
+def pivot_idx(r, i):
+    nonZeroIdx = np.nonzero(r)[0]
+    print(nonZeroIdx)
+    print(len(r))
+    val = float('inf') if len(nonZeroIdx) == 0 else nonZeroIdx[0]
+    print(val)
+    return val
+
+# Function to check if matrix is in ref
+def isREF(A):
+    pivotIdxs = np.array([pivot_idx(A[i, :], i) for i in range(A.shape[0])])
+    print(pivotIdxs)
+    if (np.ediff1d(pivotIdxs) <= 0).any():
+        return False
+    return True
+    
+A = np.array([[1, 0, 2, 0], [0, 4, 6, 4], [0, 0, 7, 8]])
+print(A, '\n', ['The matrix is NOT in REF', 'The matrix is  in REF'][isREF(A)])
+A = np.array([[0, 0, 2, 0], [0, 4, 6, 4], [0, 0, 7, 8]])
+print(A, '\n', ['The matrix is NOT in REF', 'The matrix is  in REF'][isREF(A)])
