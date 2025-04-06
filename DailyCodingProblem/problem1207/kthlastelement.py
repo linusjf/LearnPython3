86f2471aa814487490890aad860e00eee32f4017
Linus Fernandes <linus.fernandes@gmail.com>
2025-04-06 09:00:03 +0530

    fix:

--- DailyCodingProblem/problem1207/kthlastelement.py
+++ DailyCodingProblem/problem1207/kthlastelement.py
@@ -5,43 +5,49 @@ To solve this problem in one pass and constant space, we can use two pointers th
 Here's a Python implementation:
 """
 import logging
+from typing import Optional
 logging.basicConfig(level=logging.WARNING)
 
 
 class ListNode:
-    val: object
-    next: 'ListNode | None'
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+  x: object
+  next: Optional['ListNode']
+  def __init__(self, x: object):
+    self.val: object = x
+    self.next: Optional[ListNode] = None
 
-def remove_kth_last(head, k):
-    dummy = ListNode(0)
-    dummy.next = head
-    first = dummy
-    second = dummy
+def remove_kth_last(head: Optional[ListNode], k: int) -> Optional[ListNode]:
+  if (k < 1):
+    logging.warning("k (%d) must be a +ve integer.", k)
+    return head
 
-    for _ in range(k + 1):
-      if not second:
-        logging.warning("k (%d) is larger than list length", k)
-        return head
-      second = second.next
+  dummy = ListNode(0)
+  dummy.next = head
+  first = dummy
+  second = dummy
 
-    while second:
-        assert first.next is not None
-        first = first.next
-        second = second.next
+  for _ in range(k + 1):
+    if second is not None and second.next is None:
+      logging.warning("k (%d) is larger than list length", k)
+      return head
+    assert second is not None
+    second = second.next
 
+  while second:
     assert first.next is not None
-    first.next = first.next.next
-    return dummy.next
+    first = first.next
+    second = second.next
+
+  assert first.next is not None
+  first.next = first.next.next
+  return dummy.next
 
 # Example usage
-def print_list(head):
-    while head:
-        print(head.val, end=" ")
-        head = head.next
-    print()
+def print_list(head: Optional[ListNode]) -> None:
+  while head:
+    print(head.val, end=" ")
+    head = head.next
+  print()
 
 # Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
 head = ListNode(1)
@@ -60,6 +66,12 @@ print_list(head)
 
 head = remove_kth_last(head, 8)
 print("List after attempting removing the 8th last element:")
+print_list(head)
+head = remove_kth_last(head, 0)
+print("List after attempting removing the 0th last element:")
+print_list(head)
+head = remove_kth_last(head, -1)
+print("List after attempting removing the -1th last element:")
 print_list(head)
 """
 In this implementation, the `remove_kth_last` function uses two pointers to find the kth last element in the list and removes it. The `print_list` function is used to print the elements of the linked list.
